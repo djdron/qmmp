e634ad65f2986dbbdacd909765bdf2cce6f23130
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2007-09-15 19:02:05 +0000

    updated Czech translations
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@156 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp/lib/qmmp/Output/oss/translations/oss_plugin_cs.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<defaultcodec></defaultcodec>
+<context>
+    <name>OutputOSSFactory</name>
+    <message>
+        <location filename="../outputossfactory.cpp" line="30"/>
+        <source>OSS Plugin</source>
+        <translation>Plugin OSS</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="47"/>
+        <source>About OSS Output Plugin</source>
+        <translation>O pluginu OSS</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="48"/>
+        <source>Qmmp OSS Output Plugin</source>
+        <translation>Výstupní plugin Qmmp OSS</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="49"/>
+        <source>Writen by: Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</source>
+        <translation>Autor: Jurij Žuravljov &lt;slalkerg@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="50"/>
+        <source>Based on code by:Brad Hughes &lt;bhughes@trolltech.com&gt;</source>
+        <translation>Založeno na kódu Brada Hughese &lt;bhughes@trolltech.com&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="202"/>
+        <source>OSS Plugin Settings</source>
+        <translation>Nastavení pluginu OSS</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="207"/>
+        <source>Device Settings</source>
+        <translation>Nastavení zařízení</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="203"/>
+        <source>Audio device</source>
+        <translation>Zvukové zařízení</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="205"/>
+        <source>Mixer device</source>
+        <translation>Ovládání hlasitosti</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="213"/>
+        <source>Advanced Settings</source>
+        <translation>Pokročilá nastavení</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="208"/>
+        <source>Soundcard</source>
+        <translation>Zvuková karta</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="209"/>
+        <source>Buffer time (ms):</source>
+        <translation>Velikost bufferu (ms):</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="210"/>
+        <source>Period time (ms):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="212"/>
+        <source>PCM over Master</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="214"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+    <message>
+        <location filename="../ui_settingsdialog.h" line="215"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
