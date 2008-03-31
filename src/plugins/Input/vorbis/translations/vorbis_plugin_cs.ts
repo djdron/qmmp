85c0c74e2ae285b5e0c1e37f62938b746574d411
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-03-31 16:58:26 +0000

    vorbis plugin translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@317 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/vorbis/translations/vorbis_plugin_cs.ts
+++ src/plugins/Input/vorbis/translations/vorbis_plugin_cs.ts
@@ -1,164 +1,164 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru">
+<!DOCTYPE TS><TS version="1.1" language="pl">
 <context>
     <name>DecoderVorbisFactory</name>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="31"/>
         <source>Ogg Vorbis Plugin</source>
-        <translation>Модуль Ogg Vorbis</translation>
+        <translation>Plugin Ogg-Vorbis</translation>
     </message>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="33"/>
         <source>Ogg Vorbis Files</source>
-        <translation>Файлы Ogg Vorbis</translation>
+        <translation>Soubory Ogg-Vorbis</translation>
     </message>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="90"/>
         <source>About Ogg Vorbis Audio Plugin</source>
-        <translation>Об аудио-модуле Ogg Vorbis</translation>
+        <translation>O pluginu Ogg-Vorbis</translation>
     </message>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="91"/>
         <source>Qmmp Ogg Vorbis Audio Plugin</source>
-        <translation>Аудио-модуль Ogg Vorbis для Qmmp</translation>
+        <translation>Vstupní plugin Qmmp Ogg-Vorbis</translation>
     </message>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="92"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
+        <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../decodervorbisfactory.cpp" line="94"/>
         <source>Source code based on mq3 progect</source>
-        <translation>Исходный код основан на проекте mq3</translation>
+        <translation>Zdrojový kód je založen na projektu mq3</translation>
     </message>
 </context>
 <context>
     <name>DetailsDialog</name>
     <message>
-        <location filename="../detailsdialog.cpp" line="61"/>
-        <source>Hz</source>
-        <translation>Гц</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="273"/>
-        <source>Save</source>
-        <translation>Сохранить</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="283"/>
-        <source>Track number:</source>
-        <translation>Номер трека:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="296"/>
-        <source>Year:</source>
-        <translation>Год:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="306"/>
-        <source>Genre:</source>
-        <translation>Жанр:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="316"/>
-        <source>Comment:</source>
-        <translation>Комментарий:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="326"/>
-        <source>Album:</source>
-        <translation>Альбом:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="336"/>
-        <source>Artist:</source>
-        <translation>Исполнитель:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="346"/>
-        <source>Title:</source>
-        <translation>Название:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="387"/>
-        <source>Close</source>
-        <translation>Закрыть</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="197"/>
-        <source>-</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="227"/>
-        <source>File size:</source>
-        <translation>Размер файла:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="100"/>
-        <source>Sample rate:</source>
-        <translation>Дискретизация:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="19"/>
-        <source>File path:</source>
-        <translation>Путь к файлу:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="42"/>
-        <source>Ogg Vorbis Info</source>
-        <translation>Информация Ogg Vorbis</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="83"/>
-        <source>Length:</source>
-        <translation>Длительность:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="120"/>
-        <source>Channels:</source>
-        <translation>Каналов:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="143"/>
-        <source>Bit Rate</source>
-        <translation>Битовая частота</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="187"/>
-        <source>Minimum:</source>
-        <translation>Минимальная:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="204"/>
-        <source>Maximum:</source>
-        <translation>Максимальная:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="214"/>
-        <source>Nominal:</source>
-        <translation>Номинальная:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="246"/>
-        <source>Ogg Vorbis Tag</source>
-        <translation>Оgg Vorbis-тег</translation>
-    </message>
-    <message>
         <location filename="../detailsdialog.cpp" line="69"/>
         <source>kbps</source>
-        <translation>Кб/с</translation>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.cpp" line="61"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
     </message>
     <message>
         <location filename="../detailsdialog.cpp" line="70"/>
         <source>KB</source>
-        <translation>Кб</translation>
+        <translation>KB</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="13"/>
         <source>Details</source>
-        <translation>Информация</translation>
+        <translation>Podrobnosti</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="227"/>
+        <source>File size:</source>
+        <translation>Velikost souboru:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="197"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="100"/>
+        <source>Sample rate:</source>
+        <translation>Vzorkovací frekvence:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="273"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="283"/>
+        <source>Track number:</source>
+        <translation>Číslo stopy:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="296"/>
+        <source>Year:</source>
+        <translation>Rok:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="306"/>
+        <source>Genre:</source>
+        <translation>Žánr:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="316"/>
+        <source>Comment:</source>
+        <translation>Komentář:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="326"/>
+        <source>Album:</source>
+        <translation>Album:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="336"/>
+        <source>Artist:</source>
+        <translation>Umělec:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="346"/>
+        <source>Title:</source>
+        <translation>Název:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="387"/>
+        <source>Close</source>
+        <translation>Zavřít</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="19"/>
+        <source>File path:</source>
+        <translation>Cesta k souboru:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="42"/>
+        <source>Ogg Vorbis Info</source>
+        <translation>Informace Ogg-Vorbis</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="83"/>
+        <source>Length:</source>
+        <translation>Délka:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="120"/>
+        <source>Channels:</source>
+        <translation>Počet kanálů:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="143"/>
+        <source>Bit Rate</source>
+        <translation>Datový tok</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="187"/>
+        <source>Minimum:</source>
+        <translation>Minimální:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="204"/>
+        <source>Maximum:</source>
+        <translation>Maximální:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="214"/>
+        <source>Nominal:</source>
+        <translation>Nominální:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="246"/>
+        <source>Ogg Vorbis Tag</source>
+        <translation>Ogg-Vorbis tag</translation>
     </message>
 </context>
 </TS>
