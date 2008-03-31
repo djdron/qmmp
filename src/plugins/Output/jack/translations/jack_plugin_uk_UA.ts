dc8a8e64e1b84c484a361986387d42ae6a41a47f
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-03-31 17:05:14 +0000

    jack plugin translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@319 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/jack/translations/jack_plugin_uk_UA.ts
+++ src/plugins/Output/jack/translations/jack_plugin_uk_UA.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru">
+<!DOCTYPE TS><TS version="1.1" language="uk">
 <context>
     <name>OutputJACKFactory</name>
     <message>
@@ -10,17 +10,17 @@
     <message>
         <location filename="../outputjackfactory.cpp" line="47"/>
         <source>About Jack Output Plugin</source>
-        <translation>О модуле вывода Jack</translation>
+        <translation type="unfinished">Про модуль виводу Jack</translation>
     </message>
     <message>
         <location filename="../outputjackfactory.cpp" line="48"/>
         <source>Qmmp Jack Output Plugin</source>
-        <translation>Модуль вывода Jack для Qmmp</translation>
+        <translation type="unfinished">Модуль виводу Jack для Qmmp</translation>
     </message>
     <message>
         <location filename="../outputjackfactory.cpp" line="49"/>
         <source>Writen by: Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</source>
-        <translation>Разработчик: Юрий Журавлёв &lt;slalkerg@gmail.com&gt;</translation>
+        <translation type="unfinished">Розробник: Юрій Журавльов &lt;slalkerg@gmail.com&gt;</translation>
     </message>
 </context>
 </TS>
--- src/plugins/Output/jack/translations/jack_plugin_zh_CN.ts
+++ src/plugins/Output/jack/translations/jack_plugin_zh_CN.ts
@@ -1,26 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru">
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
 <context>
     <name>OutputJACKFactory</name>
     <message>
         <location filename="../outputjackfactory.cpp" line="30"/>
         <source>JACK Plugin</source>
-        <translation>Модуль JACK</translation>
+        <translation>JACK 插件</translation>
     </message>
     <message>
         <location filename="../outputjackfactory.cpp" line="47"/>
         <source>About Jack Output Plugin</source>
-        <translation>О модуле вывода Jack</translation>
+        <translation>关于 Jack 输出插件</translation>
     </message>
     <message>
         <location filename="../outputjackfactory.cpp" line="48"/>
         <source>Qmmp Jack Output Plugin</source>
-        <translation>Модуль вывода Jack для Qmmp</translation>
+        <translation>Qmmp Jack 输出插件</translation>
     </message>
     <message>
         <location filename="../outputjackfactory.cpp" line="49"/>
         <source>Writen by: Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</source>
-        <translation>Разработчик: Юрий Журавлёв &lt;slalkerg@gmail.com&gt;</translation>
+        <translation>作者：Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</translation>
     </message>
 </context>
 </TS>
