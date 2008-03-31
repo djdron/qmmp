b87f472fe953ebc7699bd5da37761f3d64483fc6
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-03-31 16:49:31 +0000

    flac plugin translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@314 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/flac/translations/flac_plugin_zh_TW.ts
+++ src/plugins/Input/flac/translations/flac_plugin_zh_TW.ts
@@ -1,31 +1,31 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ru">
+<!DOCTYPE TS><TS version="1.1" language="zh_TW">
 <context>
     <name>DecoderFLACFactory</name>
     <message>
         <location filename="../decoderflacfactory.cpp" line="26"/>
         <source>FLAC Plugin</source>
-        <translation>Модуль FLAC</translation>
+        <translation>FLAC 插件</translation>
     </message>
     <message>
         <location filename="../decoderflacfactory.cpp" line="28"/>
         <source>FLAC Files</source>
-        <translation>Файлы FLAC</translation>
+        <translation>FLAC 檔案</translation>
     </message>
     <message>
         <location filename="../decoderflacfactory.cpp" line="82"/>
         <source>About FLAC Audio Plugin</source>
-        <translation>Об аудио-модуле FLAC</translation>
+        <translation>關于 FLAC 聲訊插件</translation>
     </message>
     <message>
         <location filename="../decoderflacfactory.cpp" line="83"/>
         <source>Qmmp FLAC Audio Plugin</source>
-        <translation>Аудио-модуль FLAC для Qmmp</translation>
+        <translation>Qmmp FLAC 聲訊插件</translation>
     </message>
     <message>
         <location filename="../decoderflacfactory.cpp" line="84"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
+        <translation>作者：Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 <context>
@@ -33,122 +33,122 @@
     <message>
         <location filename="../detailsdialog.cpp" line="61"/>
         <source>Hz</source>
-        <translation>Гц</translation>
+        <translation>Hz</translation>
     </message>
     <message>
         <location filename="../detailsdialog.cpp" line="67"/>
         <source>bits</source>
-        <translation>бит</translation>
+        <translation>bits</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="42"/>
         <source>FLAC Info</source>
-        <translation>Информация FLAC</translation>
+        <translation>FLAC 資訊</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="183"/>
         <source>-</source>
-        <translation></translation>
+        <translation>-</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="86"/>
         <source>Length:</source>
-        <translation>Длительность:</translation>
+        <translation>長度：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="103"/>
         <source>Sample rate:</source>
-        <translation>Частота сэмплов:</translation>
+        <translation>取様率：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="123"/>
         <source>Channels:</source>
-        <translation>Каналов:</translation>
+        <translation>音響通道：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="136"/>
         <source>File size:</source>
-        <translation>Размер файла:</translation>
+        <translation>檔案大小：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="146"/>
         <source>Bitrate:</source>
-        <translation>Битовая частота:</translation>
+        <translation>位速率：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="173"/>
         <source>Sample width:</source>
-        <translation>Ширина кадра:</translation>
+        <translation>取様頻寬：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="19"/>
         <source>File path:</source>
-        <translation>Путь к файлу:</translation>
+        <translation>檔案路徑：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="322"/>
         <source>Close</source>
-        <translation>Закрыть</translation>
+        <translation>關閉</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="199"/>
         <source>FLAC Tag</source>
-        <translation>FLAC-тег</translation>
+        <translation>FLAC 標簽</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="299"/>
         <source>Save</source>
-        <translation>Сохранить</translation>
+        <translation>儲存</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="270"/>
         <source>Track number:</source>
-        <translation>Номер трека:</translation>
+        <translation>軌迹：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="257"/>
         <source>Year:</source>
-        <translation>Год:</translation>
+        <translation>年代：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="283"/>
         <source>Genre:</source>
-        <translation>Жанр:</translation>
+        <translation>流派：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="244"/>
         <source>Comment:</source>
-        <translation>Комментарий:</translation>
+        <translation>備注：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="231"/>
         <source>Album:</source>
-        <translation>Альбом:</translation>
+        <translation>專輯：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="218"/>
         <source>Artist:</source>
-        <translation>Исполнитель:</translation>
+        <translation>兿術家：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="205"/>
         <source>Title:</source>
-        <translation>Название:</translation>
+        <translation>標題：</translation>
     </message>
     <message>
         <location filename="../detailsdialog.cpp" line="65"/>
         <source>kbps</source>
-        <translation>Кб/с</translation>
+        <translation>kbps</translation>
     </message>
     <message>
         <location filename="../detailsdialog.cpp" line="68"/>
         <source>KB</source>
-        <translation>Кб</translation>
+        <translation>KB</translation>
     </message>
     <message>
         <location filename="../detailsdialog.ui" line="13"/>
         <source>Details</source>
-        <translation>Информация</translation>
+        <translation>詳細資料</translation>
     </message>
 </context>
 </TS>
