20919ef09a6c0828fcd75fc55023fb46d005cc09
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-03-31 16:51:59 +0000

    mpc plugin translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@315 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp/src/plugins/Input/mpc/translations/mpc_plugin_cs.ts
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<context>
+    <name>DecoderMPCFactory</name>
+    <message>
+        <location filename="../decodermpcfactory.cpp" line="26"/>
+        <source>Musepack Plugin</source>
+        <translation>Musepack plugin</translation>
+    </message>
+    <message>
+        <location filename="../decodermpcfactory.cpp" line="28"/>
+        <source>Musepack Files</source>
+        <translation>Soubory Musepack</translation>
+    </message>
+    <message>
+        <location filename="../decodermpcfactory.cpp" line="82"/>
+        <source>About Musepack Audio Plugin</source>
+        <translation>O pluginu Musepack</translation>
+    </message>
+    <message>
+        <location filename="../decodermpcfactory.cpp" line="83"/>
+        <source>Qmmp Musepack Audio Plugin</source>
+        <translation>Vstupní plugin Qmmp Musepack</translation>
+    </message>
+    <message>
+        <location filename="../decodermpcfactory.cpp" line="84"/>
+        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>DetailsDialog</name>
+    <message>
+        <location filename="../detailsdialog.cpp" line="63"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.cpp" line="59"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.cpp" line="66"/>
+        <source>KB</source>
+        <translation>KB</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="13"/>
+        <source>Details</source>
+        <translation>Podrobnosti</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="136"/>
+        <source>File size:</source>
+        <translation>Velikost souboru:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="183"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="103"/>
+        <source>Sample rate:</source>
+        <translation>Vzorkovací frekvence:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="299"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="270"/>
+        <source>Track number:</source>
+        <translation>Číslo stopy:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="257"/>
+        <source>Year:</source>
+        <translation>Rok:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="283"/>
+        <source>Genre:</source>
+        <translation>Žánr:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="244"/>
+        <source>Comment:</source>
+        <translation>Komentář:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="231"/>
+        <source>Album:</source>
+        <translation>Album:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="218"/>
+        <source>Artist:</source>
+        <translation>Umělec:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="205"/>
+        <source>Title:</source>
+        <translation>Název:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="322"/>
+        <source>Close</source>
+        <translation>Zavřít</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="19"/>
+        <source>File path:</source>
+        <translation>Cesta k souboru:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="86"/>
+        <source>Length:</source>
+        <translation>Délka:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="123"/>
+        <source>Channels:</source>
+        <translation>Počet kanálů:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="42"/>
+        <source>Musepack Info</source>
+        <translation>Informace Musepack</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="146"/>
+        <source>Bitrate:</source>
+        <translation>Datový tok:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="173"/>
+        <source>Stream version:</source>
+        <translation>Verze proudu:</translation>
+    </message>
+    <message>
+        <location filename="../detailsdialog.ui" line="199"/>
+        <source>APE Tag</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
