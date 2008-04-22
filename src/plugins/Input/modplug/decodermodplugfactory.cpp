3bd29c1e38182945961689fc87ccb22dc3f7c644
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-04-22 11:45:03 +0000

    modplug plugin: fixed it, mod, stm, s3m support
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@350 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/modplug/decodermodplugfactory.cpp
+++ src/plugins/Input/modplug/decodermodplugfactory.cpp
@@ -1,4 +1,5 @@
 #include <QtGui>
+#include <QStringList>
 
 #include <libmodplug/stdafx.h>
 #include <libmodplug/it_defs.h>
@@ -17,57 +18,13 @@ bool DecoderModPlugFactory::supports(const QString &source) const
 {
     QString lExt = source.section(".",-1).toLower();
     lExt.prepend(".");
-    if (lExt == ".xm")
-        return TRUE;
-    if (lExt == ".amf")
-        return TRUE;
-    if (lExt == ".ams")
-        return TRUE;
-    if (lExt == ".dbm")
-        return TRUE;
-    if (lExt == ".dbf")
-        return TRUE;
-    if (lExt == ".dsm")
-        return TRUE;
-    if (lExt == ".far")
-        return TRUE;
-    if (lExt == ".mdl")
-        return TRUE;
-    if (lExt == ".stm")
-        return TRUE;
-    if (lExt == ".ult")
-        return TRUE;
-    if (lExt == ".j2b")
-        return TRUE;
-    if (lExt == ".mt2")
-        return TRUE;
-    if (lExt == ".mdz")
-        return TRUE;
-    /*if (lExt == ".mdr")
-        return TRUE;*/
-    if (lExt == ".mdgz")
-        return TRUE;
-    if (lExt == ".mdbz")
-        return TRUE;
-    if (lExt == ".s3z")
-        return TRUE;
-    /*if (lExt == ".s3r")
-        return TRUE;*/
-    if (lExt == ".s3gz")
-        return TRUE;
-    if (lExt == ".xmz")
-        return TRUE;
-    /*if (lExt == ".xmr")
-        return TRUE;*/
-    if (lExt == ".xmgz")
-        return TRUE;
-    if (lExt == ".itz")
-        return TRUE;
-    /*if (lExt == ".itr")
-        return TRUE;*/
-    if (lExt == ".itgz")
-        return TRUE;
-    if (lExt == ".dmf")
+    QStringList lExtList;
+    lExtList << ".amf" << ".ams" << ".dbm" << ".dbf"  << ".dsm" << ".far" << ".mdl"
+    << ".stm" << ".ult" << ".ult" << ".j2b" << ".mt2" << ".mdz" /*<< ".mdr"*/ << ".mdgz"
+    << ".mdbz"  << ".mod" << ".s3z" << /*".s3r" <<*/ ".s3gz" << ".s3m" << ".xmz" /*<< ".xmr"*/
+    << ".xmgz" << ".itz"  << /*".itr" <<*/ ".itgz" << ".dmf" << ".umx" << ".it" << ".669"
+    << ".xm" << ".mtm" << ".psm" << ".ft2";
+    if (lExtList.contains(lExt))
         return TRUE;
     return FALSE;
 }
@@ -82,8 +39,8 @@ const DecoderProperties DecoderModPlugFactory::properties() const
     DecoderProperties properties;
     properties.name = tr("ModPlug Plugin");
     properties.filter = (QString) "*.amf *.ams *.dbm *.dbf *.dsm *.far *.mdl *.stm *.ult" +
-    "*.j2b *.mt2 *.mdz *.mdr *.mdgz *.mdbz *.mod *.s3z *.s3r *.s3gz *.s3m *.xmz"+
-    "*.xmr *.xmgz *.itz *.itr *.itgz *.dmf *.umx *.it *.669 *.xm *.mtm *.psm *.ft2";
+                        "*.j2b *.mt2 *.mdz *.mdr *.mdgz *.mdbz *.mod *.s3z *.s3r *.s3gz *.s3m *.xmz"+
+                        "*.xmr *.xmgz *.itz *.itr *.itgz *.dmf *.umx *.it *.669 *.xm *.mtm *.psm *.ft2";
     properties.description = tr("ModPlug Files");
     //properties.contentType = ;
     properties.hasAbout = TRUE;
@@ -92,7 +49,7 @@ const DecoderProperties DecoderModPlugFactory::properties() const
 }
 
 Decoder *DecoderModPlugFactory::create(QObject *parent, QIODevice *input,
-                                   Output *output)
+                                       Output *output)
 {
     return new DecoderModPlug(parent, this, input, output);
 }
@@ -101,21 +58,21 @@ FileTag *DecoderModPlugFactory::createTag(const QString &source)
 {
     FileTag *ftag = new FileTag();
     QSettings settings(QDir::homePath()+"/.qmmp/qmmprc", QSettings::IniFormat);
-    if(settings.value("UseFileName", FALSE).toBool())
+    if (settings.value("UseFileName", FALSE).toBool())
     {
         ftag->setValue(FileTag::TITLE, source.section('/',-1));
         return ftag;
     }
-    ArchiveReader reader(this);
+    ArchiveReader reader(0);
     QByteArray buffer;
-    if(reader.isSupported(source))
+    if (reader.isSupported(source))
     {
         buffer = reader.unpack(source);
     }
     else
     {
         QFile file(source);
-        if(!file.open(QIODevice::ReadOnly))
+        if (!file.open(QIODevice::ReadOnly))
         {
             qWarning("DecoderModPlugFactory: error: %s", qPrintable(file.errorString ()));
             return ftag;
@@ -151,7 +108,7 @@ void DecoderModPlugFactory::showAbout(QWidget *parent)
     QMessageBox::about (parent, tr("About ModPlug Audio Plugin"),
                         tr("Qmmp ModPlug Audio Plugin")+"\n"+
                         tr("Writen by: Ilya Kotov <forkotov02@hotmail.ru>")+"\n"+
-                        tr("Based on the Modplug Plugin for Xmms")+"\n"+ 
+                        tr("Based on the Modplug Plugin for Xmms")+"\n"+
                         tr("Modplug Plugin developers:")+"\n"+
                         tr("Olivier Lapicque <olivierl@jps.net>")+"\n"+
                         tr("Kenton Varda <temporal@gauge3d.org>")+"\n"+
