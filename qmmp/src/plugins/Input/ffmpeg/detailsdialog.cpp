0fe9f94d1bb7204786b247f1a64cd536b92e3de5
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-05-06 17:09:33 +0000

    FFMEG plugin: fixed build again
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@365 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Input/ffmpeg/detailsdialog.cpp
+++ qmmp/src/plugins/Input/ffmpeg/detailsdialog.cpp
@@ -18,11 +18,24 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-extern "C"
-{
+extern "C"{
+#if defined HAVE_FFMPEG_AVFORMAT_H
+#include <ffmpeg/avformat.h>
+#elif defined HAVE_LIBAVFORMAT_AVFORMAT_H
+#include <libavformat/avformat.h>
+#else
 #include <avformat.h>
+#endif
+
+#if defined HAVE_FFMPEG_AVCODEC_H
+#include <ffmpeg/avcodec.h>
+#elif defined HAVE_LIBAVCODEC_AVCODEC_H
+#include <libavcodec/avcodec.h>
+#else
 #include <avcodec.h>
+#endif
 }
+
 #include <QFile>
 
 #include "detailsdialog.h"
