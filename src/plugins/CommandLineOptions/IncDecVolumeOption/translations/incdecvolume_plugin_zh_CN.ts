d2b5b33546ff9468c7907ab506bcee3b25ebaba9
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-06-17 12:05:21 +0000

    updated Chinese Traditional Translation (author: lon)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@425 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/CommandLineOptions/IncDecVolumeOption/translations/incdecvolume_plugin_zh_CN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
+<defaultcodec></defaultcodec>
+<context>
+    <name>IncDecVolumeCommandLineOption</name>
+    <message>
+        <location filename="../incdecvolumeoption.cpp" line="22"/>
+        <source>Increase volume with step 5</source>
+        <translation>增大声音(5个单位)</translation>
+    </message>
+    <message>
+        <location filename="../incdecvolumeoption.cpp" line="23"/>
+        <source>Decrease volume with step 5</source>
+        <translation>降低声音(5个单位)</translation>
+    </message>
+</context>
+</TS>
