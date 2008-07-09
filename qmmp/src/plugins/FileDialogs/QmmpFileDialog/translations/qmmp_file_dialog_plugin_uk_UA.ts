75a761bcfbacab1282dbff170d6e62d122d1a745
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2008-07-09 19:44:07 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@445 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/FileDialogs/QmmpFileDialog/translations/qmmp_file_dialog_plugin_uk_UA.ts
+++ qmmp/src/plugins/FileDialogs/QmmpFileDialog/translations/qmmp_file_dialog_plugin_uk_UA.ts
@@ -1,56 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>QmmpFileDialog</name>
     <message>
         <location filename="../qmmpfiledialog.ui" line="13"/>
         <source>Add Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати файли</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="37"/>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Вгору</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="97"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="50"/>
         <source>List view</source>
-        <translation type="unfinished"></translation>
+        <translation>Дивитись перелік</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="69"/>
         <source>Detailed view</source>
-        <translation type="unfinished"></translation>
+        <translation>Детальний перегляд</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="94"/>
         <source>Close dialog on add</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрити діалог додавання</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="168"/>
         <source>File name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ім&apos;я файлу:</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="184"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="191"/>
         <source>Files of type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Типи файлів:</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.ui" line="217"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрити</translation>
     </message>
 </context>
 <context>
@@ -58,24 +59,26 @@
     <message>
         <location filename="../qmmpfiledialog.cpp" line="107"/>
         <source>Qmmp File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Діалог файлів Qmmp</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.cpp" line="106"/>
         <source>About Qmmp File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Про діалог файлів Qmmp</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.cpp" line="110"/>
         <source>Writen by:
 Vladimir Kuznetsov &lt;vovanec@gmail.com&gt;
 Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробники:
+Володимир Кузнєцов &lt;vovanec@gmail.com&gt;
+Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialog.cpp" line="111"/>
         <source>Some code is copied from the Qt library</source>
-        <translation type="unfinished"></translation>
+        <translation>Деякий код скопійовано з бібліотеки Qt</translation>
     </message>
 </context>
 <context>
@@ -83,27 +86,27 @@ Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
     <message>
         <location filename="../qmmpfiledialogimpl.cpp" line="215"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialogimpl.cpp" line="228"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Зберегти</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialogimpl.cpp" line="240"/>
         <source>Directories</source>
-        <translation type="unfinished"></translation>
+        <translation>Теки</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialogimpl.cpp" line="384"/>
         <source>already exists.</source>
-        <translation type="unfinished"></translation>
+        <translation>вже існує.</translation>
     </message>
     <message>
         <location filename="../qmmpfiledialogimpl.cpp" line="385"/>
         <source>Do you want to replace it?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ви бажаєте замінити?</translation>
     </message>
 </context>
 </TS>
--- qmmp/src/qmmpui/translations/libqmmpui_uk_UA.ts
+++ qmmp/src/qmmpui/translations/libqmmpui_uk_UA.ts
@@ -1,11 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>QtFileDialogFactory</name>
     <message>
         <location filename="../qtfiledialog.cpp" line="36"/>
         <source>Qt File Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Файловий діалог Qt</translation>
     </message>
 </context>
 </TS>
