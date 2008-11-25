250ff23d1c1e598d86a813b6826cc0fc0276002d
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2008-11-25 21:01:33 +0000

    Fixed Ukrainian translation (MPRIS Plugin)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@636 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/mpris/translations/mpris_plugin_uk_UA.ts
+++ qmmp/src/plugins/General/mpris/translations/mpris_plugin_uk_UA.ts
@@ -1,26 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>MPRISFactory</name>
     <message>
         <location filename="../mprisfactory.cpp" line="29"/>
         <source>MPRIS Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль MPRIS</translation>
     </message>
     <message>
         <location filename="../mprisfactory.cpp" line="48"/>
         <source>About MPRIS Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль MPRIS</translation>
     </message>
     <message>
         <location filename="../mprisfactory.cpp" line="50"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished">Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../mprisfactory.cpp" line="49"/>
         <source>Qmmp MPRIS Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль MPRIS для Qmmp</translation>
     </message>
 </context>
 </TS>
