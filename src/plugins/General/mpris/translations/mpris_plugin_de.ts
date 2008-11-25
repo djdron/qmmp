216d114725a18d93fbfc5cf88948205aae8db0a1
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2008-11-25 20:42:43 +0000

    added mpris plugin translations
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@634 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/General/mpris/translations/mpris_plugin_de.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de">
+<context>
+    <name>MPRISFactory</name>
+    <message>
+        <location filename="../mprisfactory.cpp" line="29"/>
+        <source>MPRIS Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mprisfactory.cpp" line="48"/>
+        <source>About MPRIS Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mprisfactory.cpp" line="50"/>
+        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished">Autor: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mprisfactory.cpp" line="49"/>
+        <source>Qmmp MPRIS Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
