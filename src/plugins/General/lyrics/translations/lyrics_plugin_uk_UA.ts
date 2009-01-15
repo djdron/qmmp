cfa24d6291cb1dcf01438a3718a3f0d45c5d2c47
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-01-15 20:29:04 +0000

    updated Ukrainian translation (lyrics plugin)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@746 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/lyrics/translations/lyrics_plugin_uk_UA.ts
+++ src/plugins/General/lyrics/translations/lyrics_plugin_uk_UA.ts
@@ -1,11 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>Lyrics</name>
     <message>
         <location filename="../lyrics.cpp" line="35"/>
         <source>View Lyrics</source>
-        <translation type="unfinished"></translation>
+        <translation>Огляд текстів</translation>
     </message>
 </context>
 <context>
@@ -13,27 +14,27 @@
     <message>
         <location filename="../lyricsfactory.cpp" line="29"/>
         <source>Lyrics Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль текстів</translation>
     </message>
     <message>
         <location filename="../lyricsfactory.cpp" line="48"/>
         <source>About Lyrics Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль текстів</translation>
     </message>
     <message>
         <location filename="../lyricsfactory.cpp" line="49"/>
         <source>Qmmp Lyrics Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль текстів для Qmmp</translation>
     </message>
     <message>
         <location filename="../lyricsfactory.cpp" line="51"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../lyricsfactory.cpp" line="50"/>
         <source>This plugin retrieves lyrics from LyricWiki.org</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль отримує тексти з LyricWiki.org</translation>
     </message>
 </context>
 <context>
@@ -41,52 +42,52 @@
     <message>
         <location filename="../lyricswindow.cpp" line="32"/>
         <source>Lyrics: %1 - %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Тексти: %1 - %2</translation>
     </message>
     <message>
         <location filename="../lyricswindow.ui" line="38"/>
         <source>No connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Немає з&apos;єднання</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="62"/>
         <source>Looking up host...</source>
-        <translation type="unfinished"></translation>
+        <translation>Пошук хоста...</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="65"/>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation>З&apos;єднання...</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="68"/>
         <source>Sending request...</source>
-        <translation type="unfinished"></translation>
+        <translation>Відсилання запиту...</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="71"/>
         <source>Receiving</source>
-        <translation type="unfinished"></translation>
+        <translation>Отримання</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="74"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>З&apos;єднано</translation>
     </message>
     <message>
         <location filename="../lyricswindow.cpp" line="77"/>
         <source>Closing connection...</source>
-        <translation type="unfinished"></translation>
+        <translation>Закриття з&apos;єднання...</translation>
     </message>
     <message>
         <location filename="../lyricswindow.ui" line="13"/>
         <source>Lyrics Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль текстів</translation>
     </message>
     <message>
         <location filename="../lyricswindow.ui" line="58"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрити</translation>
     </message>
 </context>
 </TS>
