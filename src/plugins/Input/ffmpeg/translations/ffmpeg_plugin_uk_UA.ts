4704ad03f6f50ae7565a5d5db20816a61a65c05c
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-01-29 18:49:25 +0000

    updated Ukrainian translations
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@763 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/translations/ffmpeg_plugin_uk_UA.ts
+++ src/plugins/Input/ffmpeg/translations/ffmpeg_plugin_uk_UA.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>DecoderFFmpegFactory</name>
     <message>
@@ -191,7 +192,7 @@
     <message>
         <location filename="../settingsdialog.ui" line="86"/>
         <source>MP3 (MPEG audio layer 3)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
