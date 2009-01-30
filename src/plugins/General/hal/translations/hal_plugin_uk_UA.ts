7638d2939353d580e9a4bbfe2947d71a49c6ce62
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-01-30 19:23:10 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@768 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/hal/translations/hal_plugin_uk_UA.ts
+++ src/plugins/General/hal/translations/hal_plugin_uk_UA.ts
@@ -1,36 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>HalFactory</name>
     <message>
         <location filename="../halfactory.cpp" line="30"/>
         <source>HAL Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль HAL</translation>
     </message>
     <message>
         <location filename="../halfactory.cpp" line="50"/>
         <source>About HAL Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль HAL</translation>
     </message>
     <message>
         <location filename="../halfactory.cpp" line="51"/>
         <source>Qmmp HAL Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль HAL для Qmmp</translation>
     </message>
     <message>
         <location filename="../halfactory.cpp" line="52"/>
         <source>This plugin provides removable devices detection using HAL</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль відстежує з&apos;ємні пристрої за допомогою HAL</translation>
     </message>
     <message>
         <location filename="../halfactory.cpp" line="53"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished">Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../halfactory.cpp" line="54"/>
         <source>Based on Solid (KDE hardware library)</source>
-        <translation type="unfinished"></translation>
+        <translation>Базується на базі Solid (бібліотека KDE)</translation>
     </message>
 </context>
 <context>
@@ -38,12 +39,12 @@
     <message>
         <location filename="../halplugin.cpp" line="160"/>
         <source>Add CD &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати CD &quot;%1&quot;</translation>
     </message>
     <message>
         <location filename="../halplugin.cpp" line="166"/>
         <source>Add Volume &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Додати том &quot;%1&quot;</translation>
     </message>
 </context>
 <context>
@@ -51,37 +52,37 @@
     <message>
         <location filename="../settingsdialog.ui" line="13"/>
         <source>HAL Plugin Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштування модуля HAL</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="28"/>
         <source>CD Audio Detection</source>
-        <translation type="unfinished"></translation>
+        <translation>Автовизначення аудіо-CD</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="37"/>
         <source>Add tracks to playlist automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматично додавати треки до списку</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="44"/>
         <source>Remove tracks from playlist automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматично видаляти треки зі списку</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="54"/>
         <source>Removable Devcie Detection</source>
-        <translation type="unfinished"></translation>
+        <translation>Автовизначення з&apos;ємних пристроїв</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="63"/>
         <source>Add files to playlist automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматично додавати файли до списку</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="70"/>
         <source>Remove files from playlist automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматично видаляти файли зі списку</translation>
     </message>
 </context>
 </TS>
