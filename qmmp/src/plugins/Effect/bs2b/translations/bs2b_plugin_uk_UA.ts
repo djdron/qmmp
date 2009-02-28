935897c50517fe455846f9ec128e6e8f6a8b330f
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-02-28 15:01:21 +0000

    updated Ukrainian translation (BS2B Plugin)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@811 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Effect/bs2b/translations/bs2b_plugin_uk_UA.ts
+++ qmmp/src/plugins/Effect/bs2b/translations/bs2b_plugin_uk_UA.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>EffectBs2bFactory</name>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="30"/>
         <source>BS2B Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль BS2B</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="50"/>
         <source>About BS2B Effect Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль ефектів BS2B</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="51"/>
         <source>This is the Qmmp plugin version of Boris Mikhaylov&apos;s headphone DSP effect &quot;Bauer stereophonic-to-binaural&quot;, abbreviated bs2b.</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль додає ефект &quot;Bauer stereophonic-to-binaural&quot; (скорочено bs2b) Бориса Михайлова.</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="53"/>
         <source>Visit %1 for more details</source>
-        <translation type="unfinished"></translation>
+        <translation>Для отримання інформації відвідайте %1</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="55"/>
         <source>Compiled against libbs2b-%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Зібрано з libbs2b-%1</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="56"/>
         <source>Developers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробники:</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="57"/>
         <source>Ilya Kotov</source>
-        <translation type="unfinished"></translation>
+        <translation>Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../effectbs2bfactory.cpp" line="58"/>
         <source>Sebastian Pipping</source>
-        <translation type="unfinished"></translation>
+        <translation>Sebastian Pipping &lt;sebastian@pipping.org&gt;</translation>
     </message>
 </context>
 <context>
@@ -49,42 +49,42 @@
     <message>
         <location filename="../settingsdialog.cpp" line="33"/>
         <source>low</source>
-        <translation type="unfinished"></translation>
+        <translation>низький</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="34"/>
         <source>middle</source>
-        <translation type="unfinished"></translation>
+        <translation>середній</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="35"/>
         <source>high</source>
-        <translation type="unfinished"></translation>
+        <translation>високий</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="36"/>
         <source>low (easy version)</source>
-        <translation type="unfinished"></translation>
+        <translation>низький (спрощена версія)</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="37"/>
         <source>middle (easy version)</source>
-        <translation type="unfinished"></translation>
+        <translation>середній (спрощена версія)</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="38"/>
         <source>hight (easy version)</source>
-        <translation type="unfinished"></translation>
+        <translation>високий (спрощена версія)</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>BS2B Plugin Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштування модуля BS2B</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="29"/>
         <source>Crossfeed level:</source>
-        <translation type="unfinished"></translation>
+        <translation>Рівень взаємозмішування:</translation>
     </message>
 </context>
 </TS>
