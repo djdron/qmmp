9fbc650b70eea471ae6f3984588f768067c0b35e
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-03-01 08:23:39 +0000

    updated Ukrainian translation (projectM)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@813 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Visual/projectm/translations/projectm_plugin_uk_UA.ts
+++ src/plugins/Visual/projectm/translations/projectm_plugin_uk_UA.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>ProjectMPlugin</name>
     <message>
         <location filename="../projectmplugin.cpp" line="41"/>
         <source>projectM</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -14,82 +14,82 @@
     <message>
         <location filename="../projectmwidget.cpp" line="126"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Довідка</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="126"/>
         <source>F1</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="127"/>
         <source>&amp;Show Song Title</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Показати назву пісні</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="127"/>
         <source>F2</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="128"/>
         <source>&amp;Show Preset Name</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Показати ім&apos;я предустановки</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="128"/>
         <source>F3</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="130"/>
         <source>&amp;Next Preset</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Наступна предустановка</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="130"/>
         <source>N</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="131"/>
         <source>&amp;Previous Preset</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Попередня предустановка</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="131"/>
         <source>P</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="132"/>
         <source>&amp;Random Preset</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Випадкова предустановка</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="132"/>
         <source>R</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="133"/>
         <source>&amp;Lock Preset</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Заблокувати предустановку</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="133"/>
         <source>L</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="135"/>
         <source>&amp;Fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Повноекранний режим</translation>
     </message>
     <message>
         <location filename="../projectmwidget.cpp" line="135"/>
         <source>F</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -97,32 +97,32 @@
     <message>
         <location filename="../visualprojectmfactory.cpp" line="28"/>
         <source>ProjectM</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../visualprojectmfactory.cpp" line="48"/>
         <source>About ProjectM Visual Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль ProjectM для Qmmp</translation>
     </message>
     <message>
         <location filename="../visualprojectmfactory.cpp" line="49"/>
         <source>Qmmp ProjectM Visual Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль projectM для Qmmp</translation>
     </message>
     <message>
         <location filename="../visualprojectmfactory.cpp" line="50"/>
         <source>This plugins adds projectM visualization</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль додає візуалізацію projectM</translation>
     </message>
     <message>
         <location filename="../visualprojectmfactory.cpp" line="51"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../visualprojectmfactory.cpp" line="52"/>
         <source>Based on llibrojectM-qt library</source>
-        <translation type="unfinished"></translation>
+        <translation>Базується на бібліотеці libprojectM-qt</translation>
     </message>
 </context>
 </TS>
