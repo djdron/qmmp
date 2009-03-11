49d2f1e96b72225da194f3a07b2d4f3bf7742003
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-03-11 18:03:10 +0000

    fixed Russian translation updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@838 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Input/mplayer/translations/mplayer_plugin_uk_UA.ts
+++ qmmp/src/plugins/Input/mplayer/translations/mplayer_plugin_uk_UA.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="uk">
+<!DOCTYPE TS><TS version="1.1" language="uk">
 <defaultcodec></defaultcodec>
 <context>
     <name>DecoderMplayerFactory</name>
@@ -43,18 +42,6 @@
         <translation>Закрити</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="58"/>
-        <location filename="../detailsdialog.ui" line="72"/>
-        <location filename="../detailsdialog.ui" line="86"/>
-        <location filename="../detailsdialog.ui" line="127"/>
-        <location filename="../detailsdialog.ui" line="147"/>
-        <location filename="../detailsdialog.ui" line="167"/>
-        <location filename="../detailsdialog.ui" line="187"/>
-        <location filename="../detailsdialog.ui" line="207"/>
-        <location filename="../detailsdialog.ui" line="227"/>
-        <location filename="../detailsdialog.ui" line="262"/>
-        <location filename="../detailsdialog.ui" line="282"/>
-        <location filename="../detailsdialog.ui" line="302"/>
         <location filename="../detailsdialog.ui" line="322"/>
         <source>-</source>
         <translation>-</translation>
@@ -100,7 +87,6 @@
         <translation>Роздільність:</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="140"/>
         <location filename="../detailsdialog.ui" line="295"/>
         <source>Bitrate:</source>
         <translation>Бітрейт:</translation>
@@ -116,7 +102,6 @@
         <translation>Кадрів в секунду:</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="200"/>
         <location filename="../detailsdialog.ui" line="249"/>
         <source>Codec:</source>
         <translation>Кодек:</translation>
@@ -150,11 +135,6 @@
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.cpp" line="31"/>
-        <location filename="../settingsdialog.cpp" line="39"/>
-        <location filename="../settingsdialog.cpp" line="48"/>
-        <location filename="../settingsdialog.cpp" line="49"/>
-        <location filename="../settingsdialog.cpp" line="63"/>
         <location filename="../settingsdialog.cpp" line="64"/>
         <source>default</source>
         <translation>за умовчанням</translation>
@@ -177,12 +157,12 @@
     <message>
         <location filename="../settingsdialog.ui" line="57"/>
         <source>Audio/video auto synchronization</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматична синхронізація аудіо/відео</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="67"/>
         <source>Synchronization factor:</source>
-        <translation type="unfinished"></translation>
+        <translation>Фактор синхронізації:</translation>
     </message>
 </context>
 </TS>
