5f621b184214ce1de61015a0bcc013acd5b7a96b
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-04-15 16:54:09 +0000

    added Ukrainian translation for waveout plugin
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@908 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/waveout/translations/waveout_plugin_uk_UA.ts
+++ src/plugins/Output/waveout/translations/waveout_plugin_uk_UA.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>OutputWaveOutFactory</name>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="30"/>
         <source>WaveOut Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль WaveOut</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="53"/>
         <source>About WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль виведення WaveOut</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="54"/>
         <source>Qmmp WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль виведення WaveOut для Qmmp</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="55"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="56"/>
         <source>Based on aacDECdrop player</source>
-        <translation type="unfinished"></translation>
+        <translation>Базується на програвачі aacDECdrop</translation>
     </message>
 </context>
 </TS>
