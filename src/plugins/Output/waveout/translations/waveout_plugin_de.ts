e9ef2b779995980b8fbb098162ff4409a42babcb
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-04-18 16:02:19 +0000

    updated German translation (Panagiotis Papadopoulos)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@913 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Output/waveout/translations/waveout_plugin_de.ts
+++ src/plugins/Output/waveout/translations/waveout_plugin_de.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE">
 <context>
     <name>OutputWaveOutFactory</name>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="30"/>
         <source>WaveOut Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>WaveOut-Plugin</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="53"/>
         <source>About WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Über WaveOut-Ausgabe-Plugin</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="54"/>
         <source>Qmmp WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Qmmp WaveOut-Ausgabe-Plugin</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="55"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="56"/>
