0b3e64482a75ac1bb5bb92e7d8675a8ce75d8351
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-05-11 16:13:29 +0000

    updated Ukrainian translation for fileops plugin
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@941 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/fileops/translations/fileops_plugin_uk_UA.ts
+++ src/plugins/General/fileops/translations/fileops_plugin_uk_UA.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>FileOps</name>
     <message>
         <location filename="../fileops.cpp" line="97"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Помилка</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="98"/>
         <source>Destination directory doesn&apos;t exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Тека призначення не існує</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="103"/>
         <source>Copying</source>
-        <translation type="unfinished"></translation>
+        <translation>Копіювання</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="104"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Зупинити</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="134"/>
         <source>Copying file %1/%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Копіювання файла %1/%2</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="175"/>
         <source>Remove files</source>
-        <translation type="unfinished"></translation>
+        <translation>Видалити файли</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="176"/>
         <source>Are you sure you want to remove %1 file(s) from disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Ви впевнені, що бажаєте видалити %1 файл(ів) з диску</translation>
     </message>
 </context>
 <context>
@@ -44,22 +44,22 @@
     <message>
         <location filename="../fileopsfactory.cpp" line="30"/>
         <source>File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль файлових операцій</translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="50"/>
         <source>About File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль файлових операцій</translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="51"/>
         <source>Qmmp File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль роботи з файлами для Qmmp</translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="52"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 <context>
@@ -67,146 +67,140 @@
     <message>
         <location filename="../hotkeydialog.ui" line="14"/>
         <source>Modify Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Поміняти комбінацію клавіш</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="29"/>
         <source>Press the key combination you want to assign</source>
-        <translation type="unfinished"></translation>
+        <translation>Натисніть клавіші, комбінацію яких ви бажаєте використовувати</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="43"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Очистити</translation>
     </message>
 </context>
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.cpp" line="51"/>
         <location filename="../settingsdialog.cpp" line="131"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Копіювати</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.cpp" line="52"/>
         <location filename="../settingsdialog.cpp" line="132"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Переіменувати</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.cpp" line="54"/>
         <location filename="../settingsdialog.cpp" line="134"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Видалити</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="137"/>
         <source>New action</source>
-        <translation type="unfinished"></translation>
+        <translation>Нова дія</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="191"/>
         <source>Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Виконавець</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="192"/>
         <source>Album</source>
-        <translation type="unfinished"></translation>
+        <translation>Альбом</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="193"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Назва</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="194"/>
         <source>Tracknumber</source>
-        <translation type="unfinished"></translation>
+        <translation>Номер треку</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="195"/>
         <source>Genre</source>
-        <translation type="unfinished"></translation>
+        <translation>Жанр</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="196"/>
         <source>Filename</source>
-        <translation type="unfinished"></translation>
+        <translation>Ім&apos;я файлу</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="197"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Дата</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="198"/>
         <source>Year</source>
-        <translation type="unfinished"></translation>
+        <translation>Рік</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="199"/>
         <source>Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Коментар</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="215"/>
         <source>Choose a directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Виберіть теку</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>File Operations Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштування файлових операцій</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="48"/>
         <source>Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Увімкнено</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="53"/>
         <source>Operation</source>
-        <translation type="unfinished"></translation>
+        <translation>Операція</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="58"/>
         <source>Menu text</source>
-        <translation type="unfinished"></translation>
+        <translation>Текст меню</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="63"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Комбінація клавіш</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="79"/>
         <source>+</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="92"/>
         <source>-</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="105"/>
-        <location filename="../settingsdialog.ui" line="121"/>
-        <location filename="../settingsdialog.ui" line="158"/>
         <location filename="../settingsdialog.ui" line="175"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="148"/>
         <source>Destination:</source>
-        <translation type="unfinished"></translation>
+        <translation>Призначення:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="165"/>
         <source>File name pattern:</source>
-        <translation type="unfinished"></translation>
+        <translation>Формат імені файла:</translation>
     </message>
 </context>
 </TS>
