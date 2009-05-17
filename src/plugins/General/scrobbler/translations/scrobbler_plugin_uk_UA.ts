4f70549230b21d9c7bb11c6fd43274cdad4ecf1c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-05-17 20:58:30 +0000

    libre.fm support
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@949 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/scrobbler/translations/scrobbler_plugin_uk_UA.ts
+++ src/plugins/General/scrobbler/translations/scrobbler_plugin_uk_UA.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="uk">
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
 <context>
     <name>ScrobblerFactory</name>
     <message>
@@ -26,17 +27,22 @@
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.ui" line="13"/>
+        <location filename="../settingsdialog.ui" line="14"/>
         <source>Scrobbler Plugin Settings</source>
         <translation>Налаштування модуля scrobbler</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="19"/>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Service:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="45"/>
         <source>User name:</source>
         <translation>Ім&apos;я користувача:</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="29"/>
+        <location filename="../settingsdialog.ui" line="55"/>
         <source>Password:</source>
         <translation>Пароль:</translation>
     </message>
