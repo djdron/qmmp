a7449808aab5813a72e87dcdb27aa3bc7d29faa3
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-05-17 21:02:51 +0000

    fixed translation list
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@950 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/scrobbler/translations/scrobbler_plugin_it.ts
+++ qmmp/src/plugins/General/scrobbler/translations/scrobbler_plugin_it.ts
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="it">
-<defaultcodec></defaultcodec>
 <context>
     <name>ScrobblerFactory</name>
     <message>
@@ -28,17 +27,22 @@
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.ui" line="13"/>
+        <location filename="../settingsdialog.ui" line="14"/>
         <source>Scrobbler Plugin Settings</source>
         <translation>Impostazioni del modulo Scrobbler</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="19"/>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Service:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="45"/>
         <source>User name:</source>
         <translation>Utente:</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="29"/>
+        <location filename="../settingsdialog.ui" line="55"/>
         <source>Password:</source>
         <translation>Passowrd:</translation>
     </message>
