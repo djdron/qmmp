807d4a694f9af2cbcb5645eed7b591020a301881
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-05-31 07:33:43 +0000

    updated Polish translation (Gregorz Gibas)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@959 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/fileops/translations/fileops_plugin_pl.ts
+++ src/plugins/General/fileops/translations/fileops_plugin_pl.ts
@@ -1,42 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="pl_PL">
 <context>
     <name>FileOps</name>
     <message>
         <location filename="../fileops.cpp" line="97"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="98"/>
         <source>Destination directory doesn&apos;t exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Katalog docelowy nie istnieje</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="103"/>
         <source>Copying</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiowanie</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="104"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="134"/>
         <source>Copying file %1/%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiowanie pliku %1/%2</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="175"/>
         <source>Remove files</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń pliki</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="176"/>
         <source>Are you sure you want to remove %1 file(s) from disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Jesteś pewien, że chcesz usunąć plik(i) %1 z twardego dysku</translation>
     </message>
 </context>
 <context>
@@ -44,22 +44,22 @@
     <message>
         <location filename="../fileopsfactory.cpp" line="30"/>
         <source>File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Wtyczka Operacji na plikach</translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="50"/>
         <source>About File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>O wtyczce Operacje na plikach</translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="51"/>
         <source>Qmmp File Operations Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Wtyczka Operacje na plikach dla Qmmp </translation>
     </message>
     <message>
         <location filename="../fileopsfactory.cpp" line="52"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 <context>
@@ -67,17 +67,17 @@
     <message>
         <location filename="../hotkeydialog.ui" line="14"/>
         <source>Modify Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Modyfikuj skrót klawiszowy</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="29"/>
         <source>Press the key combination you want to assign</source>
-        <translation type="unfinished"></translation>
+        <translation>Wciśnij kombinację klawiszy, które chcesz przypisać</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="43"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyczyść</translation>
     </message>
 </context>
 <context>
@@ -86,109 +86,109 @@
         <location filename="../settingsdialog.cpp" line="51"/>
         <location filename="../settingsdialog.cpp" line="131"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopiuj</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="52"/>
         <location filename="../settingsdialog.cpp" line="132"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmień nazwę</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="54"/>
         <location filename="../settingsdialog.cpp" line="134"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="137"/>
         <source>New action</source>
-        <translation type="unfinished"></translation>
+        <translation>Nowa akcja</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="191"/>
         <source>Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Artysta</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="192"/>
         <source>Album</source>
-        <translation type="unfinished"></translation>
+        <translation>Album</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="193"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Tytuł</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="194"/>
         <source>Tracknumber</source>
-        <translation type="unfinished"></translation>
+        <translation>Numer utworu</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="195"/>
         <source>Genre</source>
-        <translation type="unfinished"></translation>
+        <translation>Gatunek</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="196"/>
         <source>Filename</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa pliku</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="197"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Data</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="198"/>
         <source>Year</source>
-        <translation type="unfinished"></translation>
+        <translation>Rok</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="199"/>
         <source>Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Komentarz</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="215"/>
         <source>Choose a directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz katalog</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>File Operations Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia wtyczki Operacje na plikach</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="48"/>
         <source>Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktywne</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="53"/>
         <source>Operation</source>
-        <translation type="unfinished"></translation>
+        <translation>Operacja</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="58"/>
         <source>Menu text</source>
-        <translation type="unfinished"></translation>
+        <translation>Tekst w menu</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="63"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Skróŧ klawiszowy</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="79"/>
         <source>+</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="92"/>
         <source>-</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="105"/>
@@ -196,12 +196,12 @@
         <location filename="../settingsdialog.ui" line="158"/>
         <location filename="../settingsdialog.ui" line="175"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="148"/>
         <source>Destination:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cel:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="165"/>
--- src/plugins/Output/waveout/translations/waveout_plugin_pl.ts
+++ src/plugins/Output/waveout/translations/waveout_plugin_pl.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="pl_PL">
 <context>
     <name>OutputWaveOutFactory</name>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="30"/>
         <source>WaveOut Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Wtyczka WaveOut</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="53"/>
         <source>About WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>O wtyczce WaveOut</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="54"/>
         <source>Qmmp WaveOut Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Wtyczka WaveOut dla Qmmp</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="55"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../outputwaveoutfactory.cpp" line="56"/>
         <source>Based on aacDECdrop player</source>
-        <translation type="unfinished"></translation>
+        <translation>Oparta na kodzie odtwarzacza aacDECdrop</translation>
     </message>
 </context>
 </TS>
