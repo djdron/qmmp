ebfcec25f6435e93d8a2cdf7b71198a3e0bef1f2
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-07-10 18:04:42 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1019 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/statusicon/translations/statusicon_plugin_ru.ts
+++ src/plugins/General/statusicon/translations/statusicon_plugin_ru.ts
@@ -26,7 +26,7 @@
     <message>
         <location filename="../settingsdialog.ui" line="86"/>
         <source>Use standard icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Использовать стандартные иконки</translation>
     </message>
     <message>
         <source>Cancel</source>
