16732ecd8d20da8497526fbf1aac67c004a715f7
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-07-12 12:25:51 +0000

    fixed regression
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@1021 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/qmmp/statehandler.cpp
+++ qmmp/src/qmmp/statehandler.cpp
@@ -97,7 +97,10 @@ void StateHandler::dispatch(const QMap<Qmmp::MetaData, QString> &metaData)
             tmp.remove(tmp.key(value));
     }
     if(tmp.isEmpty() && SoundCore::instance()->url() == m_metaData.value(Qmmp::URL)) //skip empty tags
+    {
+        m_mutex.unlock();
         return;
+    }
     tmp.insert(Qmmp::URL, SoundCore::instance()->url());
     if (m_metaData != tmp)
     {
