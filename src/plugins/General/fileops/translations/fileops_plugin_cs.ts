329922857a9e308d0bc5fc79f64e1977be2db670
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-08-27 16:29:11 +0000

    updated Czech translation (Karel Volný)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1160 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/fileops/translations/fileops_plugin_cs.ts
+++ src/plugins/General/fileops/translations/fileops_plugin_cs.ts
@@ -1,43 +1,45 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="cs">
 <context>
     <name>FileOps</name>
     <message>
         <location filename="../fileops.cpp" line="97"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="98"/>
         <source>Destination directory doesn&apos;t exist</source>
-        <translation type="unfinished"></translation>
+        <translation>Cílový adresář neexistuje</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="103"/>
         <source>Copying</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopíruje se</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="104"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Přerušit</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="134"/>
         <source>Copying file %1/%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopíruje se soubor %1/%2</translation>
     </message>
     <message>
         <location filename="../fileops.cpp" line="176"/>
         <source>Remove files</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstranit soubory</translation>
     </message>
     <message numerus="yes">
         <location filename="../fileops.cpp" line="177"/>
         <source>Are you sure you want to remove %n file(s) from disk?</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Jste si jisti, že chcete z disku odstranit %n soubor?</numerusform>
+            <numerusform>Jste si jisti, že chcete z disku odstranit %n soubory?</numerusform>
+            <numerusform>Jste si jisti, že chcete z disku odstranit %n souborů?</numerusform>
         </translation>
     </message>
 </context>
@@ -69,17 +71,17 @@
     <message>
         <location filename="../hotkeydialog.ui" line="14"/>
         <source>Modify Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Změnit klávesovou zkratku</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="29"/>
         <source>Press the key combination you want to assign</source>
-        <translation type="unfinished"></translation>
+        <translation>Stlačte kombinaci kláves, kterou chcete přiřadit</translation>
     </message>
     <message>
         <location filename="../hotkeydialog.ui" line="43"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyčistit</translation>
     </message>
 </context>
 <context>
@@ -88,109 +90,109 @@
         <location filename="../settingsdialog.cpp" line="52"/>
         <location filename="../settingsdialog.cpp" line="132"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopírovat</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="53"/>
         <location filename="../settingsdialog.cpp" line="133"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Přejmenovat</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="55"/>
         <location filename="../settingsdialog.cpp" line="135"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstranit</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="138"/>
         <source>New action</source>
-        <translation type="unfinished"></translation>
+        <translation>Nová akce</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="192"/>
         <source>Artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Umělec</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="193"/>
         <source>Album</source>
-        <translation type="unfinished"></translation>
+        <translation>Album</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="194"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Název</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="195"/>
         <source>Tracknumber</source>
-        <translation type="unfinished"></translation>
+        <translation>Číslo stopy</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="196"/>
         <source>Genre</source>
-        <translation type="unfinished"></translation>
+        <translation>Žánr</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="197"/>
         <source>Filename</source>
-        <translation type="unfinished"></translation>
+        <translation>Jméno souboru</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="198"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="199"/>
         <source>Year</source>
-        <translation type="unfinished"></translation>
+        <translation>Rok</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="200"/>
         <source>Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>Komentář</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="216"/>
         <source>Choose a directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyberte adresář</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>File Operations Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavení operací se soubory</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="48"/>
         <source>Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Povoleno</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="53"/>
         <source>Operation</source>
-        <translation type="unfinished"></translation>
+        <translation>Operace</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="58"/>
         <source>Menu text</source>
-        <translation type="unfinished"></translation>
+        <translation>Text v nabídce</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="63"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Zkratka</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="79"/>
         <source>+</source>
-        <translation type="unfinished"></translation>
+        <translation>+</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="92"/>
         <source>-</source>
-        <translation type="unfinished"></translation>
+        <translation>-</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="105"/>
@@ -198,17 +200,17 @@
         <location filename="../settingsdialog.ui" line="158"/>
         <location filename="../settingsdialog.ui" line="175"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>…</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="148"/>
         <source>Destination:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cíl:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="165"/>
         <source>File name pattern:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vzor jména:</translation>
     </message>
 </context>
 </TS>
