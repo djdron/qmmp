7084576af36ecf96802b1aa924cb6143f27e779a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-10-09 20:15:33 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1297 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Engines/mplayer/translations/mplayer_plugin_uk_UA.ts
+++ src/plugins/Engines/mplayer/translations/mplayer_plugin_uk_UA.ts
@@ -2,148 +2,109 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="uk">
 <context>
-    <name>DecoderMplayerFactory</name>
+    <name>MplayerEngineFactory</name>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="50"/>
+        <location filename="../mplayerenginefactory.cpp" line="33"/>
         <source>Mplayer Plugin</source>
-        <translation>Модуль Mplayer</translation>
+        <translation type="unfinished">Модуль Mplayer</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="53"/>
+        <location filename="../mplayerenginefactory.cpp" line="36"/>
         <source>Video Files</source>
-        <translation>Відео файли</translation>
+        <translation type="unfinished">Відео файли</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="97"/>
-        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
-    </message>
-    <message>
-        <location filename="../decodermplayerfactory.cpp" line="94"/>
+        <location filename="../mplayerenginefactory.cpp" line="82"/>
         <source>About MPlayer Plugin</source>
-        <translation>Про модуль Mplayer</translation>
+        <translation type="unfinished">Про модуль Mplayer</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="95"/>
+        <location filename="../mplayerenginefactory.cpp" line="83"/>
         <source>Qmmp MPlayer Plugin</source>
-        <translation>Модуль Mplayer для Qmmp</translation>
+        <translation type="unfinished">Модуль Mplayer для Qmmp</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="96"/>
+        <location filename="../mplayerenginefactory.cpp" line="84"/>
         <source>This plugin uses MPlayer as backend</source>
-        <translation>Цей модуль використовує MPlayer як бекенд</translation>
+        <translation type="unfinished">Цей модуль використовує MPlayer як бекенд</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="85"/>
+        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>DetailsDialog</name>
+    <name>MplayerMetaDataModel</name>
     <message>
-        <location filename="../detailsdialog.ui" line="363"/>
-        <source>Close</source>
-        <translation>Закрити</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
+        <source>Size</source>
+        <translation type="unfinished">Розмір</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="58"/>
-        <location filename="../detailsdialog.ui" line="72"/>
-        <location filename="../detailsdialog.ui" line="86"/>
-        <location filename="../detailsdialog.ui" line="127"/>
-        <location filename="../detailsdialog.ui" line="147"/>
-        <location filename="../detailsdialog.ui" line="167"/>
-        <location filename="../detailsdialog.ui" line="187"/>
-        <location filename="../detailsdialog.ui" line="207"/>
-        <location filename="../detailsdialog.ui" line="227"/>
-        <location filename="../detailsdialog.ui" line="262"/>
-        <location filename="../detailsdialog.ui" line="282"/>
-        <location filename="../detailsdialog.ui" line="302"/>
-        <location filename="../detailsdialog.ui" line="322"/>
-        <source>-</source>
-        <translation>-</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="275"/>
-        <source>Sample rate:</source>
-        <translation>Дискретизація:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="37"/>
-        <source>File path:</source>
-        <translation>Шлях до файлу:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.cpp" line="33"/>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
         <source>KB</source>
-        <translation>Кб</translation>
+        <translation type="unfinished">Кб</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="51"/>
-        <source>Size:</source>
-        <translation>Розмір:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="76"/>
+        <source>Length</source>
+        <translation type="unfinished">Тривалість</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="65"/>
-        <source>Demuxer:</source>
-        <translation>Демультиплексор:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="78"/>
+        <source>Demuxer</source>
+        <translation type="unfinished">Демультиплексор</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="79"/>
-        <source>Length:</source>
-        <translation>Тривалість:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="81"/>
+        <source>Video format</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="102"/>
-        <source>Video</source>
-        <translation>Відео</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="83"/>
+        <source>FPS</source>
+        <translation type="unfinished">Кадрів в секунду</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="114"/>
-        <source>Resolution:</source>
-        <translation>Роздільність:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="85"/>
+        <source>Video codec</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="140"/>
-        <location filename="../detailsdialog.ui" line="295"/>
-        <source>Bitrate:</source>
-        <translation>Бітрейт:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="87"/>
+        <source>Aspect ratio</source>
+        <translation type="unfinished">Співвідношення</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="160"/>
-        <source>Format:</source>
-        <translation>Формат:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="89"/>
+        <source>Video bitrate</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="180"/>
-        <source>FPS:</source>
-        <translation>Кадрів в секунду:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="96"/>
+        <source>Audio codec</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="200"/>
-        <location filename="../detailsdialog.ui" line="249"/>
-        <source>Codec:</source>
-        <translation>Кодек:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="98"/>
+        <source>Sample rate</source>
+        <translation type="unfinished">Дискретизація</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="220"/>
-        <source>Aspect ratio:</source>
-        <translation>Співвідношення:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="100"/>
+        <source>Audio bitrate</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="237"/>
-        <source>Audio</source>
-        <translation>Аудіо</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="102"/>
+        <source>Channels</source>
+        <translation type="unfinished">Канали</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="315"/>
-        <source>Channels:</source>
-        <translation>Канали:</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="13"/>
-        <source>Details</source>
-        <translation>Детально</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="31"/>
-        <source>General information</source>
-        <translation>Головна інформація</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="104"/>
+        <source>Resolution</source>
+        <translation type="unfinished">Роздільність</translation>
     </message>
 </context>
 <context>
@@ -166,22 +127,22 @@
     <message>
         <location filename="../settingsdialog.ui" line="29"/>
         <source>Video:</source>
-        <translation>Відео:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="43"/>
         <source>Audio:</source>
-        <translation>Аудіо:</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="67"/>
+        <source>Synchronization factor:</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="57"/>
         <source>Audio/video auto synchronization</source>
         <translation>Автоматична синхронізація аудіо/відео</translation>
-    </message>
-    <message>
-        <location filename="../settingsdialog.ui" line="67"/>
-        <source>Synchronization factor:</source>
-        <translation>Фактор синхронізації:</translation>
     </message>
 </context>
 </TS>
--- src/plugins/Engines/mplayer/translations/mplayer_plugin_zh_CN.ts
+++ src/plugins/Engines/mplayer/translations/mplayer_plugin_zh_CN.ts
@@ -2,148 +2,109 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="zh_CN">
 <context>
-    <name>DecoderMplayerFactory</name>
+    <name>MplayerEngineFactory</name>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="50"/>
+        <location filename="../mplayerenginefactory.cpp" line="33"/>
         <source>Mplayer Plugin</source>
-        <translation>Mplayer 插件</translation>
+        <translation type="unfinished">Mplayer 插件</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="53"/>
+        <location filename="../mplayerenginefactory.cpp" line="36"/>
         <source>Video Files</source>
-        <translation>视频文件</translation>
+        <translation type="unfinished">视频文件</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="97"/>
-        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation>作者：Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
-    </message>
-    <message>
-        <location filename="../decodermplayerfactory.cpp" line="94"/>
+        <location filename="../mplayerenginefactory.cpp" line="82"/>
         <source>About MPlayer Plugin</source>
-        <translation>关于 MPlayer 插件</translation>
+        <translation type="unfinished">关于 MPlayer 插件</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="95"/>
+        <location filename="../mplayerenginefactory.cpp" line="83"/>
         <source>Qmmp MPlayer Plugin</source>
-        <translation>Qmmp MPlayer 插件</translation>
+        <translation type="unfinished">Qmmp MPlayer 插件</translation>
     </message>
     <message>
-        <location filename="../decodermplayerfactory.cpp" line="96"/>
+        <location filename="../mplayerenginefactory.cpp" line="84"/>
         <source>This plugin uses MPlayer as backend</source>
-        <translation>此插件使用 MPlayer 后端</translation>
+        <translation type="unfinished">此插件使用 MPlayer 后端</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="85"/>
+        <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>DetailsDialog</name>
+    <name>MplayerMetaDataModel</name>
     <message>
-        <location filename="../detailsdialog.ui" line="363"/>
-        <source>Close</source>
-        <translation>关闭</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
+        <source>Size</source>
+        <translation type="unfinished">大小：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="58"/>
-        <location filename="../detailsdialog.ui" line="72"/>
-        <location filename="../detailsdialog.ui" line="86"/>
-        <location filename="../detailsdialog.ui" line="127"/>
-        <location filename="../detailsdialog.ui" line="147"/>
-        <location filename="../detailsdialog.ui" line="167"/>
-        <location filename="../detailsdialog.ui" line="187"/>
-        <location filename="../detailsdialog.ui" line="207"/>
-        <location filename="../detailsdialog.ui" line="227"/>
-        <location filename="../detailsdialog.ui" line="262"/>
-        <location filename="../detailsdialog.ui" line="282"/>
-        <location filename="../detailsdialog.ui" line="302"/>
-        <location filename="../detailsdialog.ui" line="322"/>
-        <source>-</source>
-        <translation>-</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="275"/>
-        <source>Sample rate:</source>
-        <translation>取样率：</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="37"/>
-        <source>File path:</source>
-        <translation>文件路径：</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.cpp" line="33"/>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
         <source>KB</source>
-        <translation>KB</translation>
+        <translation type="unfinished">KB</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="51"/>
-        <source>Size:</source>
-        <translation>大小：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="76"/>
+        <source>Length</source>
+        <translation type="unfinished">长度：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="65"/>
-        <source>Demuxer:</source>
-        <translation>流分离：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="78"/>
+        <source>Demuxer</source>
+        <translation type="unfinished">流分离：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="79"/>
-        <source>Length:</source>
-        <translation>长度：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="81"/>
+        <source>Video format</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="102"/>
-        <source>Video</source>
-        <translation>视频</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="83"/>
+        <source>FPS</source>
+        <translation type="unfinished">FPS</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="114"/>
-        <source>Resolution:</source>
-        <translation>分辨率：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="85"/>
+        <source>Video codec</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="140"/>
-        <location filename="../detailsdialog.ui" line="295"/>
-        <source>Bitrate:</source>
-        <translation>位速率：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="87"/>
+        <source>Aspect ratio</source>
+        <translation type="unfinished">宽高比：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="160"/>
-        <source>Format:</source>
-        <translation>格式：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="89"/>
+        <source>Video bitrate</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="180"/>
-        <source>FPS:</source>
-        <translation>FPS:</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="96"/>
+        <source>Audio codec</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="200"/>
-        <location filename="../detailsdialog.ui" line="249"/>
-        <source>Codec:</source>
-        <translation>编解码器：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="98"/>
+        <source>Sample rate</source>
+        <translation type="unfinished">取样率：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="220"/>
-        <source>Aspect ratio:</source>
-        <translation>宽高比：</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="100"/>
+        <source>Audio bitrate</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="237"/>
-        <source>Audio</source>
-        <translation>音频</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="102"/>
+        <source>Channels</source>
+        <translation type="unfinished">声音通道：</translation>
     </message>
     <message>
-        <location filename="../detailsdialog.ui" line="315"/>
-        <source>Channels:</source>
-        <translation>声音通道：</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="13"/>
-        <source>Details</source>
-        <translation>详细资料</translation>
-    </message>
-    <message>
-        <location filename="../detailsdialog.ui" line="31"/>
-        <source>General information</source>
-        <translation>常规信息</translation>
+        <location filename="../mplayermetadatamodel.cpp" line="104"/>
+        <source>Resolution</source>
+        <translation type="unfinished">分辨率：</translation>
     </message>
 </context>
 <context>
@@ -166,22 +127,22 @@
     <message>
         <location filename="../settingsdialog.ui" line="29"/>
         <source>Video:</source>
-        <translation>视频：</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="43"/>
         <source>Audio:</source>
-        <translation>音频：</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="67"/>
+        <source>Synchronization factor:</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="57"/>
         <source>Audio/video auto synchronization</source>
         <translation>音频/视频自动同步</translation>
-    </message>
-    <message>
-        <location filename="../settingsdialog.ui" line="67"/>
-        <source>Synchronization factor:</source>
-        <translation>同步系数：</translation>
     </message>
 </context>
 </TS>
