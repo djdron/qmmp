5509d768aee8f436bae317db82602de86e885b51
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-10-31 06:18:59 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1339 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ src/plugins/General/kdenotify/translations/kdenotify_plugin_uk_UA.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
+<context>
+    <name>KdeNotify</name>
+    <message>
+        <location filename="../kdenotify.cpp" line="58"/>
+        <source>Now playing:</source>
+        <translation>Відтворюється:</translation>
+    </message>
+    <message>
+        <location filename="../kdenotify.cpp" line="68"/>
+        <source>by </source>
+        <translation>виконавець</translation>
+    </message>
+    <message>
+        <location filename="../kdenotify.cpp" line="73"/>
+        <source>on </source>
+        <translation>на</translation>
+    </message>
+</context>
+<context>
+    <name>KdeNotifyFactory</name>
+    <message>
+        <location filename="../kdenotifyfactory.cpp" line="10"/>
+        <source>KDE 4 notification plugin</source>
+        <translation>Модуль повідомлень KDE4</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>KDE 4 Notification Plugin Settings</source>
+        <translation>Налаштування модуля повідомлень KDE4</translation>
+    </message>
+    <message>
+        <source>Notification delay [ms]:</source>
+        <translation>Час показу [мс]:</translation>
+    </message>
+</context>
+</TS>
