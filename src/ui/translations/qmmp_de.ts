a6f7d41058e0b46f121fbe154ecd6af096332581
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2009-11-15 18:02:51 +0000

    fixed German translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1378 90c681e8-e032-0410-971d-27865f9a5e38

--- src/ui/translations/qmmp_de.ts
+++ src/ui/translations/qmmp_de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de">
+<TS version="2.0" language="de_DE">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -1052,7 +1052,7 @@
     <message>
         <location filename="../playlist.cpp" line="193"/>
         <source>Del</source>
-        <translation>Entf</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../playlist.cpp" line="198"/>
