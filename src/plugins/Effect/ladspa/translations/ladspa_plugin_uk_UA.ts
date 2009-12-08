b14eead53c86379297cb147239e77c4b5a7607f3
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-12-08 17:39:34 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1429 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Effect/ladspa/translations/ladspa_plugin_uk_UA.ts
+++ src/plugins/Effect/ladspa/translations/ladspa_plugin_uk_UA.ts
@@ -1,47 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="uk" sourcelanguage="en">
 <context>
     <name>EffectLADSPAFactory</name>
     <message>
         <location filename="../effectladspafactory.cpp" line="30"/>
         <source>LADSPA Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Модуль LADSPA</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="50"/>
         <source>About LADSPA Host for Qmmp</source>
-        <translation type="unfinished"></translation>
+        <translation>Про модуль LADSPA для Qmmp</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="51"/>
         <source>LADSPA Host for Qmmp</source>
-        <translation type="unfinished"></translation>
+        <translation>LADSPA Host для Qmmp</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="52"/>
         <source>Writen by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="53"/>
         <source>Based on the LADSPA Host for BMP</source>
-        <translation type="unfinished"></translation>
+        <translation>Базується на модулі LADSPA для BMP</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="54"/>
         <source>BMP-ladspa developers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Розробники BMP-ladspa:</translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="55"/>
         <source>Nick Lamb &lt;njl195@zepler.org.uk&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../effectladspafactory.cpp" line="56"/>
         <source>Giacomo Lozito &lt;city_hunter@users.sf.net&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -49,37 +49,37 @@
     <message>
         <location filename="../settingsdialog.cpp" line="42"/>
         <source>UID</source>
-        <translation type="unfinished"></translation>
+        <translation>UID</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="43"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ім&apos;я</translation>
     </message>
     <message>
         <location filename="../settingsdialog.cpp" line="112"/>
         <source>This LADSPA plugin has no user controls</source>
-        <translation type="unfinished"></translation>
+        <translation>Цей модуль не містить налаштувань</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>LADSPA Plugin Catalog</source>
-        <translation type="unfinished"></translation>
+        <translation>Каталог модулів LADSPA</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="52"/>
         <source>&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&gt;</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="59"/>
         <source>&lt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="94"/>
         <source>Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>Налаштувати</translation>
     </message>
 </context>
 </TS>
