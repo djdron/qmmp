3adcbb9b58732d08aab76037722289865e921e9a
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2009-12-19 11:15:19 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@1449 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/ui/translations/qmmp_uk_UA.ts
+++ qmmp/src/ui/translations/qmmp_uk_UA.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="uk">
-<defaultcodec></defaultcodec>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -1253,7 +1253,7 @@
     <message>
         <location filename="../playlist.cpp" line="329"/>
         <source>&amp;Delete List</source>
-        <translation></translation>
+        <translation>&amp;Видалити список</translation>
     </message>
     <message>
         <location filename="../playlist.cpp" line="336"/>
