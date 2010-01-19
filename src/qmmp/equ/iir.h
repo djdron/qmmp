74352b437a2c6274dd10f6e37e48f8d8a784d985
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2010-01-19 13:11:20 +0000

    equalizer now supports 6 channels
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1509 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/equ/iir.h
+++ src/qmmp/equ/iir.h
@@ -66,7 +66,7 @@ __inline__ int round_trick(float floatvalue_to_round);
 __inline__ int round_ppc(float x);
 #endif
 
-#define EQ_CHANNELS 2
+#define EQ_CHANNELS 6
 #define EQ_MAX_BANDS 10
 
 extern float preamp[EQ_CHANNELS];
