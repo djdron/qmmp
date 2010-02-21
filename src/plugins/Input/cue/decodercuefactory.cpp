d9914b709ee19d7a2c7838aae8f0a444e0739621
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2010-02-21 21:29:42 +0000

    cue plugin: fixed bug with error message
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@1563 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/cue/decodercuefactory.cpp
+++ src/plugins/Input/cue/decodercuefactory.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2009 by Ilya Kotov                                 *
+ *   Copyright (C) 2008-2010 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -63,6 +63,8 @@ Decoder *DecoderCUEFactory::create(const QString &path, QIODevice *input)
 QList<FileInfo *> DecoderCUEFactory::createPlayList(const QString &fileName, bool useMetaData)
 {
     Q_UNUSED(useMetaData);
+    if(!QFile::exists(fileName))
+        return QList<FileInfo *>();
     CUEParser parser(fileName);
     return parser.createPlayList();
 }
