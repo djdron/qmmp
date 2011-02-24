53e00474ff92c6a0e37bedf87f5b8e89fddad78d
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-02-24 18:53:29 +0000

    updated translation files
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2073 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/ui/translations/qmmp_es.ts
+++ qmmp/src/ui/translations/qmmp_es.ts
@@ -1799,12 +1799,12 @@
 <context>
     <name>TextScroller</name>
     <message>
-        <location filename="../textscroller.cpp" line="51"/>
+        <location filename="../textscroller.cpp" line="52"/>
         <source>Autoscroll Songname</source>
         <translation>Autodesplazar el nombre de la canción</translation>
     </message>
     <message>
-        <location filename="../textscroller.cpp" line="120"/>
+        <location filename="../textscroller.cpp" line="122"/>
         <source>Buffering: %1%</source>
         <translation type="unfinished"></translation>
     </message>
