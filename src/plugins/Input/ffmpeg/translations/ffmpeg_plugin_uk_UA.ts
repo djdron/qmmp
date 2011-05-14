4aa89bfb8cb01953da99d3f832de4bf191b81410
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2011-05-14 16:26:54 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2197 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/translations/ffmpeg_plugin_uk_UA.ts
+++ src/plugins/Input/ffmpeg/translations/ffmpeg_plugin_uk_UA.ts
@@ -112,7 +112,7 @@
     <message>
         <location filename="../settingsdialog.ui" line="103"/>
         <source>MPEG-4 AAC/ALAC</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="110"/>
