d56975b1665c1e05c5a51cf8fe2171e0c6beb3fd
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-05-20 17:24:22 +0000

    fixed cursor behaviour
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2216 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/ui/playlisttitlebar.cpp
+++ qmmp/src/ui/playlisttitlebar.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007-2009 by Ilya Kotov                                 *
+ *   Copyright (C) 2007-2011 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -172,6 +172,7 @@ void PlayListTitleBar::mouseReleaseEvent(QMouseEvent*)
 {
     Dock::instance()->updateDock();
     m_resize = false;
+    setCursor (Qt::ArrowCursor);
 }
 
 void PlayListTitleBar::mouseMoveEvent(QMouseEvent* event)
