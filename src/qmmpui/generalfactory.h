4e1aa3f1916e34480c24d54685a712424a779c2a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-07-01 19:51:03 +0000

    fixed build
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2254 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/generalfactory.h
+++ src/qmmpui/generalfactory.h
@@ -3,76 +3,64 @@
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
+ *   it under the terms of the GNU Ui Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
+ *   GNU Ui Public License for more details.                          *
  *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
+ *   You should have received a copy of the GNU Ui Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#ifndef GENERALFACTORY_H
-#define GENERALFACTORY_H
+#ifndef UiFACTORY_H
+#define UiFACTORY_H
 
 class QObject;
 class QTranslator;
 class QDialog;
 class QString;
 class QWidget;
-class Control;
-class General;
 
-/*! @brief Helper class to store general plugin properies.
+/*! @brief Helper class to store ui plugin properies.
  */
-class GeneralProperties
+class UiProperties
 {
 public:
     /*!
      * Constructor
      */
-    GeneralProperties()
+    UiProperties()
     {
         hasAbout = false;
-        hasSettings = false;
-        visibilityControl = false;
+
     }
     QString name;           /*!< File dialog plugin full name */
     QString shortName;      /*!< File dialog short name for internal usage */
     bool hasAbout;          /*!< Should be \b true if plugin has about dialog, otherwise returns \b false */
-    bool hasSettings;       /*!< Should be \b true if plugin has settings dialog, otherwise returns \b false */
-    bool visibilityControl; /*!< Should be \b true if plugin can show/hide main window of the player,
-                             * otherwise returns \b false */
 };
-/*! @brief %General plugin interface.
+/*! @brief User interface plugin interface.
  * @author Ilya Kotov <forkotov02@hotmail.ru>
  */
-class GeneralFactory
+class UiFactory
 {
 public:
     /*!
      * Object destructor.
      */
-    virtual ~GeneralFactory() {}
+    virtual ~UiFactory() {}
     /*!
-     * Returns general plugin properties.
+     * Returns ui plugin properties.
      */
-    virtual const GeneralProperties properties() const = 0;
+    virtual const UiProperties properties() const = 0;
     /*!
-     * Creates object of the General class.
+     * Creates Ui object instance.
      */
-    virtual General *create(QObject *parent) = 0;
-    /*!
-     * Creates configuration dialog.
-     * @param parent Parent widget.
-     * @return Configuration dialog pointer.
-     */
-    virtual QDialog *createConfigDialog(QWidget *parent) = 0;
+    virtual QObject *create() = 0;
     /*!
      * Shows about dialog.
      * @param parent Parent widget.
@@ -85,5 +73,5 @@ public:
     virtual QTranslator *createTranslator(QObject *parent) = 0;
 };
 
-Q_DECLARE_INTERFACE(GeneralFactory, "GeneralFactory/1.0");
+Q_DECLARE_INTERFACE(UiFactory, "UiFactory/1.0")
 #endif
