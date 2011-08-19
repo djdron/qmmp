5ab4b138361eb69503b1d194a49999e9443d7584
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2011-08-19 11:18:01 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2311 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/skinned/translations/skinned_plugin_uk_UA.ts
+++ src/plugins/Ui/skinned/translations/skinned_plugin_uk_UA.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="uk">
+<!DOCTYPE TS><TS version="1.1" language="uk">
+<defaultcodec></defaultcodec>
 <context>
     <name>ActionManager</name>
     <message>
@@ -400,7 +400,6 @@
 <context>
     <name>EqWidget</name>
     <message>
-        <location filename="../eqwidget.cpp" line="154"/>
         <location filename="../eqwidget.cpp" line="177"/>
         <source>preset</source>
         <translation>предвстановлення</translation>
@@ -670,31 +669,26 @@
         <translation>Падіння аналізатора</translation>
     </message>
     <message>
-        <location filename="../mainvisual.cpp" line="333"/>
         <location filename="../mainvisual.cpp" line="347"/>
         <source>Slowest</source>
         <translation>Найповільніше</translation>
     </message>
     <message>
-        <location filename="../mainvisual.cpp" line="334"/>
         <location filename="../mainvisual.cpp" line="348"/>
         <source>Slow</source>
         <translation>Повільне</translation>
     </message>
     <message>
-        <location filename="../mainvisual.cpp" line="335"/>
         <location filename="../mainvisual.cpp" line="349"/>
         <source>Medium</source>
         <translation>Середнє</translation>
     </message>
     <message>
-        <location filename="../mainvisual.cpp" line="336"/>
         <location filename="../mainvisual.cpp" line="350"/>
         <source>Fast</source>
         <translation>Швидке</translation>
     </message>
     <message>
-        <location filename="../mainvisual.cpp" line="337"/>
         <location filename="../mainvisual.cpp" line="351"/>
         <source>Fastest</source>
         <translation>Найшвидше</translation>
@@ -766,37 +760,31 @@
         <translation>Сортувати</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="207"/>
         <location filename="../playlist.cpp" line="242"/>
         <source>By Title</source>
         <translation>За назвою</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="211"/>
         <location filename="../playlist.cpp" line="246"/>
         <source>By Album</source>
         <translation>За альбомом</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="215"/>
         <location filename="../playlist.cpp" line="250"/>
         <source>By Artist</source>
         <translation>За артистом</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="219"/>
         <location filename="../playlist.cpp" line="254"/>
         <source>By Filename</source>
         <translation>За ім&apos;ям файлу</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="223"/>
         <location filename="../playlist.cpp" line="258"/>
         <source>By Path + Filename</source>
         <translation>За шляхом та файлом</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="227"/>
         <location filename="../playlist.cpp" line="262"/>
         <source>By Date</source>
         <translation>За датою</translation>
@@ -822,7 +810,6 @@
         <translation>&amp;Новий список</translation>
     </message>
     <message>
-        <location filename="../playlist.cpp" line="231"/>
         <location filename="../playlist.cpp" line="266"/>
         <source>By Track Number</source>
         <translation></translation>
@@ -851,13 +838,11 @@
         <translation>Новий</translation>
     </message>
     <message>
-        <location filename="../forms/playlistbrowser.ui" line="52"/>
         <location filename="../playlistbrowser.cpp" line="39"/>
         <source>Delete</source>
         <translation>Видалити</translation>
     </message>
     <message>
-        <location filename="../forms/playlistbrowser.ui" line="69"/>
         <location filename="../forms/playlistbrowser.ui" line="79"/>
         <source>...</source>
         <translation>...</translation>
@@ -1122,7 +1107,6 @@ Andrey Adreev &lt;andreev00@gmail.com&gt;</source>
         <translation>Поновити</translation>
     </message>
     <message>
-        <location filename="../forms/skinnedsettings.ui" line="89"/>
         <location filename="../forms/skinnedsettings.ui" line="95"/>
         <source>View</source>
         <translation>Вигляд</translation>
@@ -1153,13 +1137,11 @@ Andrey Adreev &lt;andreev00@gmail.com&gt;</source>
         <translation>Програвач:</translation>
     </message>
     <message>
-        <location filename="../forms/skinnedsettings.ui" line="171"/>
         <location filename="../forms/skinnedsettings.ui" line="219"/>
         <source>???</source>
         <translation>???</translation>
     </message>
     <message>
-        <location filename="../forms/skinnedsettings.ui" line="178"/>
         <location filename="../forms/skinnedsettings.ui" line="226"/>
         <source>...</source>
         <translation>...</translation>
@@ -1185,8 +1167,6 @@ Andrey Adreev &lt;andreev00@gmail.com&gt;</source>
         <translation>Головне вікно</translation>
     </message>
     <message>
-        <location filename="../forms/skinnedsettings.ui" line="272"/>
-        <location filename="../forms/skinnedsettings.ui" line="296"/>
         <location filename="../forms/skinnedsettings.ui" line="320"/>
         <source>0</source>
         <translation></translation>
@@ -1239,7 +1219,7 @@ Andrey Adreev &lt;andreev00@gmail.com&gt;</source>
     <message>
         <location filename="../forms/skinnedsettings.ui" line="430"/>
         <source>Playlist separator:</source>
-        <translation type="unfinished"></translation>
+        <translation>Розділювач списків:</translation>
     </message>
     <message>
         <location filename="../forms/skinnedsettings.ui" line="380"/>
@@ -1282,7 +1262,7 @@ Andrey Adreev &lt;andreev00@gmail.com&gt;</source>
     <message>
         <location filename="../textscroller.cpp" line="53"/>
         <source>Transparent Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Прозоре тло</translation>
     </message>
     <message>
         <location filename="../textscroller.cpp" line="122"/>
