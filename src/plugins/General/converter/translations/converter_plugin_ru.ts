c6a05ce1b524b4e5576d4d44159a1e32da329733
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-10-07 17:55:21 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2382 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/converter/translations/converter_plugin_ru.ts
+++ src/plugins/General/converter/translations/converter_plugin_ru.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../converter.cpp" line="142"/>
         <source>Track: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Трек: %1</translation>
     </message>
     <message>
         <location filename="../converter.cpp" line="144"/>
         <source>Preset: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Предустановка: %1</translation>
     </message>
 </context>
 <context>
@@ -19,32 +19,32 @@
     <message>
         <location filename="../converterdialog.ui" line="14"/>
         <source>Converter Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки преобразователя</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="29"/>
         <source>Select tracks to convert:</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите треки для преобразования:</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="43"/>
         <source>Output file directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Директория результирующего файла:</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="53"/>
         <source>Output file name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Имя выходного файла:</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="63"/>
         <source>Preset:</source>
-        <translation type="unfinished"></translation>
+        <translation>Предустановка:</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="73"/>
         <source>Overwrite existing files</source>
-        <translation type="unfinished"></translation>
+        <translation>Перезаписывать существующие файлы</translation>
     </message>
     <message>
         <location filename="../converterdialog.ui" line="87"/>
@@ -56,92 +56,92 @@
     <message>
         <location filename="../converterdialog.cpp" line="88"/>
         <source>Choose a directory</source>
-        <translation type="unfinished">Выберите директорию</translation>
+        <translation>Выберите директорию</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="107"/>
         <source>Artist</source>
-        <translation type="unfinished">Исполнитель</translation>
+        <translation>Исполнитель</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="108"/>
         <source>Album</source>
-        <translation type="unfinished">Альбом</translation>
+        <translation>Альбом</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="109"/>
         <source>Title</source>
-        <translation type="unfinished">Название</translation>
+        <translation>Название</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="110"/>
         <source>Track number</source>
-        <translation type="unfinished">Номер трека</translation>
+        <translation>Номер трека</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="111"/>
         <source>Two-digit track number</source>
-        <translation type="unfinished">2-х разрядный номер трека</translation>
+        <translation>2-х разрядный номер трека</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="112"/>
         <source>Genre</source>
-        <translation type="unfinished">Жанр</translation>
+        <translation>Жанр</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="113"/>
         <source>Comment</source>
-        <translation type="unfinished">Комментарий</translation>
+        <translation>Комментарий</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="114"/>
         <source>Composer</source>
-        <translation type="unfinished">Композитор</translation>
+        <translation>Композитор</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="115"/>
         <source>Duration</source>
-        <translation type="unfinished">Длительность</translation>
+        <translation>Длительность</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="116"/>
         <source>Disc number</source>
-        <translation type="unfinished">Номер диска</translation>
+        <translation>Номер диска</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="117"/>
         <source>File name</source>
-        <translation type="unfinished">Имя файла</translation>
+        <translation>Имя файла</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="127"/>
         <source>Create a copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Создать копию</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="118"/>
         <source>Year</source>
-        <translation type="unfinished">Год</translation>
+        <translation>Год</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="119"/>
         <source>Condition</source>
-        <translation type="unfinished">Условие</translation>
+        <translation>Условие</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="125"/>
         <source>Create</source>
-        <translation type="unfinished"></translation>
+        <translation>Создать</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="126"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменить</translation>
     </message>
     <message>
         <location filename="../converterdialog.cpp" line="128"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить</translation>
     </message>
 </context>
 <context>
@@ -172,7 +172,7 @@
     <message>
         <location filename="../converterhelper.cpp" line="35"/>
         <source>Convert</source>
-        <translation type="unfinished"></translation>
+        <translation>Конвертировать</translation>
     </message>
     <message>
         <location filename="../converterhelper.cpp" line="36"/>
@@ -187,7 +187,7 @@
     <message>
         <location filename="../converterhelper.cpp" line="43"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
 </context>
 <context>
@@ -195,27 +195,27 @@
     <message>
         <location filename="../preseteditor.ui" line="14"/>
         <source>Preset Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Редактор предустановок</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="29"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Общие</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="35"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Имя:</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="45"/>
         <source>Extension:</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширение:</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="58"/>
         <source>Command</source>
-        <translation type="unfinished"></translation>
+        <translation>Команда</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="67"/>
@@ -225,17 +225,17 @@
     <message>
         <location filename="../preseteditor.ui" line="77"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Опции</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="83"/>
         <source>Write tags</source>
-        <translation type="unfinished"></translation>
+        <translation>Прописывать теги</translation>
     </message>
     <message>
         <location filename="../preseteditor.ui" line="90"/>
         <source>Convert to 16 bit</source>
-        <translation type="unfinished"></translation>
+        <translation>Преобразовать в 16 бит</translation>
     </message>
 </context>
 </TS>
