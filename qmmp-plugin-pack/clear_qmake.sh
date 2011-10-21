4d3f1d76fa68b6c76a32cbe2cfe11b40cf340737
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-10-21 10:51:20 +0000

    inital directory structure
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2420 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp-plugin-pack/clear_qmake.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+###################################################
+# Removes remaining qmake files from qmmp package #
+###################################################
+
+make distclean
+rm -rf lib
+find . -type f -name *.qm -delete
+find . -type d -name ".build" | xargs rm -rf
