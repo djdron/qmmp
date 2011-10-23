ea875756b09a92adf90fdd00ffe6ca4b5a23b777
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2011-10-23 19:11:44 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2431 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/CommandLineOptions/PlayListOption/translations/playlist_plugin_uk_UA.ts
+++ qmmp/src/plugins/CommandLineOptions/PlayListOption/translations/playlist_plugin_uk_UA.ts
@@ -6,43 +6,43 @@
     <message>
         <location filename="../playlistoption.cpp" line="44"/>
         <source>Show playlist manipulation commands</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати команди керування списком відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="57"/>
         <source>List all available playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати всі доступні списки відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="58"/>
         <source>Show playlist content</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати вміст списка відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="59"/>
         <source>Clear playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Очистити список відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="60"/>
         <source>Toggle playlist repeat</source>
-        <translation type="unfinished"></translation>
+        <translation>Увімкнути/вимкнути повторення списка відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="61"/>
         <source>Toggle playlist shuffle</source>
-        <translation type="unfinished"></translation>
+        <translation>Увімкнути/вимкнути режим випадкового відтворення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="62"/>
         <source>Show playlist options</source>
-        <translation type="unfinished"></translation>
+        <translation>Показати налаштування списка відткорення</translation>
     </message>
     <message>
         <location filename="../playlistoption.cpp" line="81"/>
         <location filename="../playlistoption.cpp" line="95"/>
         <source>Invalid playlist ID</source>
-        <translation type="unfinished"></translation>
+        <translation>Невірний номер списка відтворення</translation>
     </message>
 </context>
 </TS>
