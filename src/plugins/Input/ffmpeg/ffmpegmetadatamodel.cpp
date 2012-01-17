64558d05f1ba545d9e850d5ce8dcd841669408d6
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-01-17 18:00:00 +0000

    legacy ffmpeg support
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2539 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/ffmpegmetadatamodel.cpp
+++ src/plugins/Input/ffmpeg/ffmpegmetadatamodel.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2011 by Ilya Kotov                                 *
+ *   Copyright (C) 2009-2012 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -19,6 +19,8 @@
  ***************************************************************************/
 
 #include "ffmpegmetadatamodel.h"
+
+#if (LIBAVCODEC_VERSION_INT >= ((53<<16)+(42<<8)+4))
 
 FFmpegMetaDataModel::FFmpegMetaDataModel(const QString &path, QObject *parent) : MetaDataModel(parent)
 {
@@ -66,3 +68,56 @@ QHash<QString, QString> FFmpegMetaDataModel::audioProperties()
     }
     return ap;
 }
+
+#else
+
+FFmpegMetaDataModel::FFmpegMetaDataModel(const QString &path, QObject *parent) : MetaDataModel(parent)
+{
+    m_in = 0;
+    avcodec_init();
+    avcodec_register_all();
+    av_register_all();
+    if (av_open_input_file(&m_in, path.toLocal8Bit(), NULL,0, NULL) < 0)
+        return;
+    av_find_stream_info(m_in);
+    av_read_play(m_in);
+}
+
+FFmpegMetaDataModel::~FFmpegMetaDataModel()
+{
+    if(m_in)
+        av_close_input_file(m_in);
+}
+
+QHash<QString, QString> FFmpegMetaDataModel::audioProperties()
+{
+    QHash<QString, QString> ap;
+    if(!m_in)
+        return ap;
+    QString text = QString("%1").arg(int(m_in->duration/AV_TIME_BASE)/60);
+    text +=":"+QString("%1").arg(int(m_in->duration/AV_TIME_BASE)%60,2,10,QChar('0'));
+    ap.insert(tr("Length"), text);
+    ap.insert(tr("File size"),  QString("%1 ").arg(m_in->file_size/1024)+" "+tr("KB"));
+    ap.insert(tr("Bitrate"), QString("%1 "+tr("kbps")).arg(m_in->bit_rate/1000));
+
+    AVCodecContext *c = 0;
+    uint wma_idx;
+    for (wma_idx = 0; wma_idx < m_in->nb_streams; wma_idx++)
+    {
+        c = m_in->streams[wma_idx]->codec;
+#if LIBAVCODEC_VERSION_MAJOR < 53
+        if (c->codec_type == CODEC_TYPE_AUDIO)
+#else
+        if (c->codec_type == AVMEDIA_TYPE_AUDIO)
+#endif
+            break;
+    }
+    if (c)
+    {
+        ap.insert(tr("Sample rate"), QString("%1 " + tr("Hz")).arg(c->sample_rate));
+        ap.insert(tr("Channels"), QString("%1").arg(c->channels));
+    }
+    return ap;
+}
+
+#endif
