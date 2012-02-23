8a7c0449947e685f0b03b6b851b94c5bc1c3993e
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-02-23 15:54:25 +0000

    fixed header
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2637 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/lyrics/lyricsfactory.h
+++ qmmp/src/plugins/General/lyrics/lyricsfactory.h
@@ -17,8 +17,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#ifndef CONVERTERFACTORY_H
-#define CONVERTERFACTORY_H
+#ifndef LYRICSFACTORY_H
+#define LYRICSFACTORY_H
 
 /**
     @author Ilya Kotov <forkotov02@hotmail.ru>
