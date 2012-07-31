9bdd31adb3b06dcd2d35d5b940eb07dd1016c2e9
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-07-31 17:51:38 +0000

    updated linguist files
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2849 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/FileDialogs/QmmpFileDialog/translations/qmmp_file_dialog_plugin_pt_BR.ts
+++ src/plugins/FileDialogs/QmmpFileDialog/translations/qmmp_file_dialog_plugin_pt_BR.ts
@@ -86,22 +86,22 @@ Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
 <context>
     <name>QmmpFileDialogImpl</name>
     <message>
-        <location filename="../qmmpfiledialogimpl.cpp" line="248"/>
+        <location filename="../qmmpfiledialogimpl.cpp" line="254"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qmmpfiledialogimpl.cpp" line="261"/>
+        <location filename="../qmmpfiledialogimpl.cpp" line="267"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qmmpfiledialogimpl.cpp" line="275"/>
+        <location filename="../qmmpfiledialogimpl.cpp" line="281"/>
         <source>Directories</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qmmpfiledialogimpl.cpp" line="444"/>
+        <location filename="../qmmpfiledialogimpl.cpp" line="450"/>
         <source>%1 already exists.
 Do you want to replace it?</source>
         <translation type="unfinished"></translation>
