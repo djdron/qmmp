9d592a97edaf3895494111f1841398281aabd59b
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-07-31 11:06:44 +0000

    fixed about dialog
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2842 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/qmmpui/aboutdialog.cpp
+++ qmmp/src/qmmpui/aboutdialog.cpp
@@ -72,7 +72,7 @@ QString AboutDialog::loadAbout()
     text.append("<p>");
     text.append(tr("(c) %1-%2 Qmmp Development Team").arg(2006).arg(2012)+"<br>");
     text.append("<a href=\"http://qmmp.ylsoftware.com/\">http://qmmp.ylsoftware.com/</a><br>");
-    text.append("<a href=\"http://code.google.com/p/qmmp//\">http://code.google.com/p/qmmp/</a>");
+    text.append("<a href=\"http://code.google.com/p/qmmp/\">http://code.google.com/p/qmmp/</a>");
     text.append("</p>");
 
     text.append("<h5>"+tr("Input plugins:")+"</h5>");
