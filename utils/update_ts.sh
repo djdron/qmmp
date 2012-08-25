3e925c75ad668cccfbaa34ab7122bf17ae76d27e
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-08-25 10:39:34 +0000

    moved some scripts to separate directory
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2897 90c681e8-e032-0410-971d-27865f9a5e38

--- utils/update_ts.sh
+++ utils/update_ts.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-for file in `find . -name "*.pro"`
+for file in `find ../ -name "*.pro"`
 do
     ts_pro=`cat $file | grep TRANSLATIONS`
     if [ "$ts_pro" ]
     then
-        lupdate -no-obsolete $file
+        lupdate -no-obsolete -silent -extensions 'cpp,ui' $file
     fi
 
 done
