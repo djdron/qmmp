36eb2e1d3b80933a84179637de78796c0c6da74a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-09-01 14:08:57 +0000

    removed useless translation string
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@2924 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/translations/libqmmpui_sk.ts
+++ src/qmmpui/translations/libqmmpui_sk.ts
@@ -559,15 +559,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../forms/detailsdialog.ui" line="71"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../detailsdialog.cpp" line="106"/>
         <source>Title</source>
         <translation type="unfinished">Názov</translation>
