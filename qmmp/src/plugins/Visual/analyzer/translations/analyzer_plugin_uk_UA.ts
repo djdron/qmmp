3c681ba06e5ec79b6d12e938d6ef40aa631130f0
motsyo <motsyo@90c681e8-e032-0410-971d-27865f9a5e38>
2012-09-30 12:37:15 +0000

    updated Ukrainian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@2956 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Visual/analyzer/translations/analyzer_plugin_uk_UA.ts
+++ qmmp/src/plugins/Visual/analyzer/translations/analyzer_plugin_uk_UA.ts
@@ -19,7 +19,7 @@
     <message>
         <location filename="../colorwidget.cpp" line="37"/>
         <source>Select Color</source>
-        <translation type="unfinished"></translation>
+        <translation>Виберіть колір</translation>
     </message>
 </context>
 <context>
