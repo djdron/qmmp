39bf42212e62549c891dd5d7bfe42b5adbe2c5f6
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-12-08 10:40:01 +0000

    fixed build
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@3042 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Output/null/outputnull.cpp
+++ qmmp/src/plugins/Output/null/outputnull.cpp
@@ -18,6 +18,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 
+#include <unistd.h>
 #include "outputnull.h"
 
 
