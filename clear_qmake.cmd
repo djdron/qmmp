e662e18e5a2cae65e8385e7ef9fbd73a80aa841a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-12-11 09:47:36 +0000

    fixed clear_qmake.cmd
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3062 90c681e8-e032-0410-971d-27865f9a5e38

--- clear_qmake.cmd
+++ clear_qmake.cmd
@@ -1,4 +1,3 @@
-make distclean
 for  /r %%B in (*.qm)  do del /s /q %%B
 for  /r %%B in (*.dll)  do del /s /q %%B
 for  /r %%B in (*.a)  do del /s /q %%B
@@ -9,6 +8,5 @@ for /d /r %%B in (debug)  do rmdir /s /q %%B
 for /d /r %%B in (release)  do rmdir /s /q %%B
 for /r %%B in (*resource.rc)  do del /s /q %%B
 for /r %%B in (*.Debug)  do del /s /q %%B
-del /s /q bin\qmmp.exe
 rmdir /s /q bin\plugins
-
+del /s /q bin\qmmp.exe
