32bfafd9760ff483614d9370a6aeef070e3ea40a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-01-08 13:47:09 +0000

    removed unused code
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3135 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/aboutdialog_p.h
+++ src/qmmpui/aboutdialog_p.h
@@ -1,5 +1,5 @@
 /***************************************************************************
-*   Copyright (C) 2006-2012 by Ilya Kotov                                 *
+*   Copyright (C) 2006-2013 by Ilya Kotov                                 *
 *   forkotov02@hotmail.ru                                                 *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -35,9 +35,6 @@ class AboutDialog : public QDialog, private Ui::AboutDialog
 public:
     AboutDialog(QWidget* parent = 0, Qt::WFlags fl = 0 );
     ~AboutDialog();
-
-protected slots:
-    virtual void accept();
 
 private:
     QString loadAbout();
