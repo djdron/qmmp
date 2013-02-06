549450361707dd49e8d06d275dab4acccf031de4
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-02-06 19:03:14 +0000

    added udisks2 plugin
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@3200 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ qmmp/src/plugins/General/udisks2/translations/udisks2_plugin_he.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="he_IL">
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>UDisks Plugin Settings</source>
+        <translation>הגדרות תוספת UDisks</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="29"/>
+        <source>CD Audio Detection</source>
+        <translation>איתור שמע אוטומטי של CD</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="38"/>
+        <source>Add tracks to playlist automatically</source>
+        <translation>הוסף רצועות אוטומטית אל רשימת השמעה</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="45"/>
+        <source>Remove tracks from playlist automatically</source>
+        <translation>הסר רצועות אוטומטית מן רשימת השמעה</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="55"/>
+        <source>Removable Device Detection</source>
+        <translation>איתור התקנים ברי הזזה</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="64"/>
+        <source>Add files to playlist automatically</source>
+        <translation>הוסף קבצים אוטומטית אל רשימת השמעה</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="71"/>
+        <source>Remove files from playlist automatically</source>
+        <translation>הסר קבצים אוטומטית מן רשימת השמעה</translation>
+    </message>
+</context>
+<context>
+    <name>UDisks2Factory</name>
+    <message>
+        <location filename="../udisks2factory.cpp" line="30"/>
+        <source>UDisks2 Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../udisks2factory.cpp" line="50"/>
+        <source>About UDisks2 Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../udisks2factory.cpp" line="51"/>
+        <source>Qmmp UDisks2 Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../udisks2factory.cpp" line="52"/>
+        <source>This plugin provides removable devices detection using UDisks2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../udisks2factory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished">חוברה על ידי: Ilya Kotov ‫&lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>UDisks2Plugin</name>
+    <message>
+        <location filename="../udisks2plugin.cpp" line="153"/>
+        <source>Add CD &quot;%1&quot;</source>
+        <translation type="unfinished">הוסף CD ‫&quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../udisks2plugin.cpp" line="160"/>
+        <source>Add Volume &quot;%1&quot;</source>
+        <translation type="unfinished">הוסף כרך &quot;%1&quot;</translation>
+    </message>
+</context>
+</TS>
