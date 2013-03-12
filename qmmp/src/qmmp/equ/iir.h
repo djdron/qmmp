20051ba230e1569e8d272fcc4739ca7ef192b9bc
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-03-12 16:43:46 +0000

    fixed gcc warning
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@3309 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/qmmp/equ/iir.h
+++ qmmp/src/qmmp/equ/iir.h
@@ -58,8 +58,8 @@ void set_preamp(int chn, float val);
 
 
 int iir(void * d, int length, int nch);
-int iir24(void * d, int length, int nch);
 int iir32(void * d, int length, int nch);
+int iir24(void * d, int length, int nch);
 
 #ifdef ARCH_X86
 __inline__ int round_trick(float floatvalue_to_round);
