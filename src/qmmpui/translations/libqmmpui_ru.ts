35e048225340c6895c4455beb2d3ca48c2596920
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-10-07 05:27:14 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3752 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/translations/libqmmpui_ru.ts
+++ src/qmmpui/translations/libqmmpui_ru.ts
@@ -247,7 +247,7 @@
     <message>
         <location filename="../configdialog.cpp" line="334"/>
         <source>Galician</source>
-        <translation type="unfinished"></translation>
+        <translation>галисийский</translation>
     </message>
     <message>
         <location filename="../configdialog.cpp" line="335"/>
