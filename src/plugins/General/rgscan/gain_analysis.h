2852e17f9d6a5ec6c3f1d4b850e23d29187e69da
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-10-10 08:40:30 +0000

    rgscan: fixed gcc warnings
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3768 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/rgscan/gain_analysis.h
+++ src/plugins/General/rgscan/gain_analysis.h
@@ -25,6 +25,13 @@
  *    http://www.replaygain.org/
  */
 
+/*
+     modifications compared to original code:
+     added full reentrancy support
+     fixed gcc warnings
+*/
+
+
 #ifndef GAIN_ANALYSIS_H
 #define GAIN_ANALYSIS_H
 
@@ -49,7 +56,6 @@ int InitGainAnalysis (GainHandle_t **handle, long samplefreq);
 int AnalyzeSamples (GainHandle_t *handle, const Float_t* left_samples, const Float_t* right_samples, size_t num_samples, int num_channels);
 int ResetSampleFrequency (GainHandle_t *handle, long samplefreq);
 Float_t GetTitleGain(GainHandle_t *handle);
-//Float_t GetAlbumGain(GainHandle_t *handle);
 Float_t GetAlbumGain(GainHandle_t **handle, int count);
 void DeinitGainAbalysis(GainHandle_t *handle);
 
