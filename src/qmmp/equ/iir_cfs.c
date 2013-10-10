60174ed521a22d6e678e357ee27fb2cc8335fcee
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-10-10 08:12:23 +0000

    added list of modifications for equalizer implementation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3766 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/equ/iir_cfs.c
+++ src/qmmp/equ/iir_cfs.c
@@ -21,6 +21,12 @@
  *   $Id: iir_cfs.c,v 1.1 2005/10/17 01:57:59 liebremx Exp $
  */
 
+/*
+     modifications compared to original code:
+     added 48/96 kHz sampling rate support
+     added 24/32bit sample size support
+*/
+
 #include "iir_cfs.h"
 #include <stdio.h>
 #include <math.h>
