c3cecc47af05e986636398a2d61c847f23987cc4
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-10-10 07:18:36 +0000

    updated linguist files
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3765 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/rgscan/translations/rgscan_plugin_he.ts
+++ src/plugins/General/rgscan/translations/rgscan_plugin_he.ts
@@ -59,31 +59,61 @@
     <name>RGScanFactory</name>
     <message>
         <location filename="../rgscanfactory.cpp" line="28"/>
-        <source>ReplayGain Scaner</source>
+        <source>ReplayGain Scanner Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="49"/>
+        <source>About ReplayGain Scanner Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="50"/>
+        <source>ReplayGain Scanner Plugin for Qmmp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="51"/>
+        <source>This plugin scans audio files and gives information for volume normalization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="53"/>
+        <source>Based on source code by:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="54"/>
+        <source>David Robinson &lt;David@Robinson.org&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="55"/>
+        <source>Glen Sawyer &lt;mp3gain@hotmail.com&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../rgscanfactory.cpp" line="56"/>
+        <source>Frank Klemm</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>RGScanHelper</name>
     <message>
-        <location filename="../rgscanhelper.cpp" line="35"/>
-        <source>Calculate ReplayGain</source>
+        <location filename="../rgscanhelper.cpp" line="33"/>
+        <source>ReplayGain Scanner</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../rgscanhelper.cpp" line="36"/>
+        <location filename="../rgscanhelper.cpp" line="34"/>
         <source>Meta+R</source>
         <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../rgscanhelper.cpp" line="42"/>
-        <source>Scaning...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../rgscanhelper.cpp" line="43"/>
-        <source>Cancel</source>
-        <translation type="unfinished">ביטול</translation>
     </message>
 </context>
 </TS>
