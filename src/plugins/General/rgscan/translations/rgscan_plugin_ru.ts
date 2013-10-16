f62f7b769b7e5aa5b59d7783c1150fcdfcee4fee
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-10-16 05:25:46 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3791 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/General/rgscan/translations/rgscan_plugin_ru.ts
+++ src/plugins/General/rgscan/translations/rgscan_plugin_ru.ts
@@ -1,74 +1,74 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru_RU" sourcelanguage="en_GB">
 <context>
     <name>RGScanDialog</name>
     <message>
         <location filename="../rgscandialog.ui" line="14"/>
         <source>ReplayGain Scanner</source>
-        <translation type="unfinished"></translation>
+        <translation>Сканер ReplayGain</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="29"/>
         <source>Write track gain/peak</source>
-        <translation type="unfinished"></translation>
+        <translation>Записывать пик/усиление для трека</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="56"/>
         <source>Title</source>
-        <translation type="unfinished">Название</translation>
+        <translation>Название</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="61"/>
         <source>Progress</source>
-        <translation type="unfinished"></translation>
+        <translation>Прогресс</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="66"/>
         <source>Track Gain</source>
-        <translation type="unfinished"></translation>
+        <translation>Усиление трека</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="71"/>
         <source>Album Gain</source>
-        <translation type="unfinished"></translation>
+        <translation>Усиление альбома</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="76"/>
         <source>Track Peak</source>
-        <translation type="unfinished"></translation>
+        <translation>Пик трека</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="81"/>
         <source>Album Peak</source>
-        <translation type="unfinished"></translation>
+        <translation>Пик альбома</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="89"/>
         <source>Calculate</source>
-        <translation type="unfinished"></translation>
+        <translation>Рассчитать</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="96"/>
         <source>Write Tags</source>
-        <translation type="unfinished"></translation>
+        <translation>Записатьтеги</translation>
     </message>
     <message>
         <location filename="../rgscandialog.ui" line="103"/>
         <source>Write album gain/peak</source>
-        <translation type="unfinished"></translation>
+        <translation>Записывать пик/усиление для альбома</translation>
     </message>
     <message>
         <location filename="../rgscandialog.cpp" line="108"/>
         <location filename="../rgscandialog.cpp" line="206"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <location filename="../rgscandialog.cpp" line="130"/>
         <location filename="../rgscandialog.cpp" line="201"/>
         <source>%1 dB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 дБ</translation>
     </message>
 </context>
 <context>
@@ -76,32 +76,32 @@
     <message>
         <location filename="../rgscanfactory.cpp" line="28"/>
         <source>ReplayGain Scanner Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль сканир. ReplayGain</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="49"/>
         <source>About ReplayGain Scanner Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О модуле сканирования ReplayGain</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="50"/>
         <source>ReplayGain Scanner Plugin for Qmmp</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Модуль сканирования ReplayGain для Qmmp</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="51"/>
         <source>This plugin scans audio files and gives information for volume normalization</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Данный модуль сканирует аудио-файлы и выдаёт информацию для нормализации громкости</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="52"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="53"/>
         <source>Based on source code by:</source>
-        <translation type="unfinished"></translation>
+        <translation>Основан на исходном коде следующих разработчиков:</translation>
     </message>
     <message>
         <location filename="../rgscanfactory.cpp" line="54"/>
@@ -124,7 +124,7 @@
     <message>
         <location filename="../rgscanhelper.cpp" line="32"/>
         <source>ReplayGain Scanner</source>
-        <translation type="unfinished"></translation>
+        <translation>Сканер ReplaGain</translation>
     </message>
     <message>
         <location filename="../rgscanhelper.cpp" line="33"/>
