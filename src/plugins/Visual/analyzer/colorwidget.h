d7afca84d6ecdebd0bf5f9d299d841f2f3a8bbcc
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-11-01 05:49:55 +0000

    fixed contact email
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3850 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Visual/analyzer/colorwidget.h
+++ src/plugins/Visual/analyzer/colorwidget.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2005-2012 by Ilya Kotov                                 *
- *   qmmeter_freedevelop@mail.ru                                           *
+ *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
