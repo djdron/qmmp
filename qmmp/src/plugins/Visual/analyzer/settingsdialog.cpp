39ddc82ba70c7c340502ad057ce746e79e43cdd5
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-11-01 07:27:49 +0000

    analyzer: added context menu
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@3851 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Visual/analyzer/settingsdialog.cpp
+++ qmmp/src/plugins/Visual/analyzer/settingsdialog.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007-2012 by Ilya Kotov                                 *
+ *   Copyright (C) 2007-2013 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -27,18 +27,16 @@ SettingsDialog::SettingsDialog(QWidget *parent) : QDialog(parent)
 {
     m_ui.setupUi(this);
     QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
-    m_ui.analyzerComboBox->setCurrentIndex(settings.value("Analyzer/analyzer_falloff", 3).toInt()-1);
-    m_ui.peaksCheckBox->setChecked(settings.value("Analyzer/show_peaks", true).toBool());
-    m_ui.peaksComboBox->setCurrentIndex(settings.value("Analyzer/peaks_falloff", 3).toInt()-1);
-    m_ui.fpsComboBox->setCurrentIndex(settings.value("Analyzer/refresh_rate", 2).toInt()-1);
-    m_ui.colorWidget1->setColor(settings.value("Analyzer/color1", "Green").toString());
-    m_ui.colorWidget2->setColor(settings.value("Analyzer/color2", "Yellow").toString());
-    m_ui.colorWidget3->setColor(settings.value("Analyzer/color3", "Red").toString());
-    m_ui.bgColorWidget->setColor(settings.value("Analyzer/bg_color", "Black").toString());
-    m_ui.peakColorWidget->setColor(settings.value("Analyzer/peak_color", "Cyan").toString());
-    QSize cells_size = settings.value("Analyzer/cells_size", QSize(15, 6)).toSize();
+    settings.beginGroup("Analyzer");
+    m_ui.colorWidget1->setColor(settings.value("color1", "Green").toString());
+    m_ui.colorWidget2->setColor(settings.value("color2", "Yellow").toString());
+    m_ui.colorWidget3->setColor(settings.value("color3", "Red").toString());
+    m_ui.bgColorWidget->setColor(settings.value("bg_color", "Black").toString());
+    m_ui.peakColorWidget->setColor(settings.value("peak_color", "Cyan").toString());
+    QSize cells_size = settings.value("cells_size", QSize(15, 6)).toSize();
     m_ui.cellWidthSpinBox->setValue(cells_size.width());
     m_ui.cellHeightSpinBox->setValue(cells_size.height());
+    settings.endGroup();
 }
 
 SettingsDialog::~SettingsDialog()
@@ -48,16 +46,14 @@ SettingsDialog::~SettingsDialog()
 void SettingsDialog::accept()
 {
     QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
-    settings.setValue("Analyzer/analyzer_falloff", m_ui.analyzerComboBox->currentIndex() + 1);
-    settings.setValue("Analyzer/peaks_falloff", m_ui.peaksComboBox->currentIndex() + 1);
-    settings.setValue("Analyzer/refresh_rate", m_ui.fpsComboBox->currentIndex() + 1);
-    settings.setValue("Analyzer/show_peaks", m_ui.peaksCheckBox->isChecked());
-    settings.setValue("Analyzer/color1", m_ui.colorWidget1->colorName());
-    settings.setValue("Analyzer/color2", m_ui.colorWidget2->colorName());
-    settings.setValue("Analyzer/color3", m_ui.colorWidget3->colorName());
-    settings.setValue("Analyzer/bg_color", m_ui.bgColorWidget->colorName());
-    settings.setValue("Analyzer/peak_color", m_ui.peakColorWidget->colorName());
-    settings.setValue("Analyzer/cells_size", QSize(m_ui.cellWidthSpinBox->value(),
+    settings.beginGroup("Analyzer");
+    settings.setValue("color1", m_ui.colorWidget1->colorName());
+    settings.setValue("color2", m_ui.colorWidget2->colorName());
+    settings.setValue("color3", m_ui.colorWidget3->colorName());
+    settings.setValue("bg_color", m_ui.bgColorWidget->colorName());
+    settings.setValue("peak_color", m_ui.peakColorWidget->colorName());
+    settings.setValue("cells_size", QSize(m_ui.cellWidthSpinBox->value(),
                                                    m_ui.cellHeightSpinBox->value()));
+    settings.endGroup();
     QDialog::accept();
 }
