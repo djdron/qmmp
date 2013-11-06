c4d1b0402742e75ca0e64b38c854bb4c8039cd0c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-11-06 11:40:22 +0000

    updated .ts files
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@3873 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/hotkey/translations/hotkey_plugin_uk_UA.ts
+++ qmmp/src/plugins/General/hotkey/translations/hotkey_plugin_uk_UA.ts
@@ -110,6 +110,11 @@
         <translation>Перекти до треку</translation>
     </message>
     <message>
+        <location filename="../settingsdialog.cpp" line="48"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>Global Hotkey Plugin Settings</source>
         <translation>Налаштування модуля глобальних клавіш</translation>
