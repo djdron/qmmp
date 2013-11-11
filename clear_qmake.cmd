b1bb0964be082e85b76ef279cdf6488c9f1d8f21
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-11-11 06:57:21 +0000

    improved clear_qmake.cmd
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@3881 90c681e8-e032-0410-971d-27865f9a5e38

--- clear_qmake.cmd
+++ clear_qmake.cmd
@@ -1,12 +1,4 @@
-for  /r %%B in (*.qm)  do del /s /q %%B
-for  /r %%B in (*.dll)  do del /s /q %%B
-for  /r %%B in (*.a)  do del /s /q %%B
-for  /r %%B in (*.Release)  do del /s /q %%B
-for  /r %%B in (Makefile*)  do del /s /q %%B
-for /d /r %%B in (.build)  do rmdir /s /q %%B
-for /d /r %%B in (debug)  do rmdir /s /q %%B
-for /d /r %%B in (release)  do rmdir /s /q %%B
-for /r %%B in (*resource.rc)  do del /s /q %%B
-for /r %%B in (*.Debug)  do del /s /q %%B
+for  /r %%B in (*.qm *.dll *.a *.Release Makefile* *resource.rc *.Debug *.exe object_script.* *.pc *.prl)  do del /s /q %%B
+for /d /r %%B in (.build debug release)  do rmdir /s /q %%B
 rmdir /s /q bin\plugins
-del /s /q bin\qmmp.exe
+
