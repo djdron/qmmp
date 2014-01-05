e0bf222733295ea053d288b80edf7c6a76510ca5
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-01-05 17:44:35 +0000

    plugin pack: added ubuntu release scripts
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@4003 90c681e8-e032-0410-971d-27865f9a5e38

---
+++ extras/package/ubuntu/qmmp-plugin-pack/qmmp_plugin_pack_deb.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+PLUGIN_PACK_VERSION=0.7.4
+UBUNTU_CODENAMES='precise quantal raring saucy'
+BUILD_ROOT=build-root
+
+build ()
+{
+    echo '****'$1'****'
+    mkdir $1
+    cd $1
+    tar xvjf ../../qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar.bz2
+    mkdir qmmp-plugin-pack-$PLUGIN_PACK_VERSION/debian
+    cp -rv ../../debian-$1/* -t qmmp-plugin-pack-$PLUGIN_PACK_VERSION/debian/
+    cp ../../qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar.bz2 ./
+    bzip2 -dv ./qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar.bz2
+    mv ./qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar ./qmmp-plugin-pack_$PLUGIN_PACK_VERSION.orig.tar
+    gzip ./qmmp-plugin-pack_$PLUGIN_PACK_VERSION.orig.tar
+    cd qmmp-plugin-pack-$PLUGIN_PACK_VERSION
+    if [ "$1" = "precise" ]; then
+        debuild -S -sa -kF594F6B4
+    else
+	debuild -S -sd -kF594F6B4
+    fi
+    cd ..
+    cd ..
+}
+
+update ()
+{
+	dch -m --newversion 0.8.0-1ubuntu1~${1}0 -D ${1} -c debian-$1/changelog "New upstream release."
+}
+
+upload ()
+{
+	dput ppa:forkotov02/ppa $1/*.changes
+}
+
+clean ()
+{
+    rm -rf $BUILD_ROOT
+    rm qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar.bz2
+}
+
+case $1 in
+    --download)
+		wget http://qmmp.ylsoftware.com/files/plugins/qmmp-plugin-pack-$PLUGIN_PACK_VERSION.tar.bz2
+    ;;
+    --update)
+		for CODENAME in $UBUNTU_CODENAMES
+		do
+			update $CODENAME
+		done
+    ;;
+    --build)
+		rm -rf $BUILD_ROOT
+		mkdir $BUILD_ROOT
+		cd $BUILD_ROOT
+		for CODENAME in $UBUNTU_CODENAMES
+		do
+			build $CODENAME
+		done
+    ;;
+    --upload)
+       cd $BUILD_ROOT
+       for CODENAME in $UBUNTU_CODENAMES
+		do
+			upload $CODENAME
+		done
+    ;;
+    --clean)
+		clean
+    ;;
+    *)
+		echo "Commands:"
+		echo "--download"
+		echo "--update"
+		echo "--build"
+		echo "--upload"
+		echo "--clean"
+esac
