5b867efbdf5e088802f9c092ff3b96501c2cf068
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-02-16 12:13:05 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@4095 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Output/directsound/translations/directsound_plugin_ru.ts
+++ qmmp/src/plugins/Output/directsound/translations/directsound_plugin_ru.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../outputdirectsoundfactory.cpp" line="29"/>
         <source>DirectSound Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль DirectSound</translation>
     </message>
     <message>
         <location filename="../outputdirectsoundfactory.cpp" line="53"/>
         <source>About DirectSound Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>О модуле вывода DirectSound</translation>
     </message>
     <message>
         <location filename="../outputdirectsoundfactory.cpp" line="54"/>
         <source>Qmmp DirectSound Output Plugin</source>
-        <translation type="unfinished"></translation>
+        <translation>Модуль вывода DirectSound для Qmmp</translation>
     </message>
     <message>
         <location filename="../outputdirectsoundfactory.cpp" line="55"/>
         <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
-        <translation type="unfinished">Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
+        <translation>Разработчик: Илья Котов &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
 </TS>
