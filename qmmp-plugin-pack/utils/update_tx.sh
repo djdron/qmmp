0892c464cc81557bbddc07fb7da15b2d2122e630
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-09-15 07:43:49 +0000

    updated transifex support (plugin pack, devel version)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@4482 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp-plugin-pack/utils/update_tx.sh
+++ qmmp-plugin-pack/utils/update_tx.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+PROJECT_NAME=qmmp-plugin-pack
 TX_CONFIG="../.tx/config"
 
 echo "[main]" > ${TX_CONFIG}
@@ -18,10 +19,20 @@ do
      echo "Updating ${plug_name}"
 
      file_filter=`echo ${tr_dir} | sed 's/..\///'`
-     echo "[qmmp-plugin-pack.${plug_name}]" >> ${TX_CONFIG}
+     echo "[${PROJECT_NAME}.${plug_name}]" >> ${TX_CONFIG}
      echo "file_filter = ${file_filter}/${plug_name}_plugin_<lang>.ts" >> ${TX_CONFIG}
      echo "source_lang = en" >> ${TX_CONFIG}
      echo "source_file = ${file_filter}/${plug_name}_plugin_en.ts" >> ${TX_CONFIG}
      echo "type = QT" >> ${TX_CONFIG}
      echo "" >> ${TX_CONFIG}
+done
+
+for RESOURCE_NAME in qsui_authors
+do
+    echo "[${PROJECT_NAME}.${RESOURCE_NAME}]" >> ${TX_CONFIG}
+    echo "file_filter = src/Ui/qsui/txt/${RESOURCE_NAME}_<lang>.txt" >> ${TX_CONFIG}
+    echo "source_lang = en" >> ${TX_CONFIG}
+    echo "source_file = src/Ui/qsui/txt/${RESOURCE_NAME}.txt" >> ${TX_CONFIG}
+    echo "type = TXT" >> ${TX_CONFIG}
+    echo "" >> ${TX_CONFIG}
 done
