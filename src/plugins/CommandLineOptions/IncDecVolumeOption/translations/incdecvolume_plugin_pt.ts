3acae019284dcc0f1722397dc4c685d3684a65f1
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-09-15 09:59:30 +0000

    updated Portuguese translation (Sérgio Marques)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@4485 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/CommandLineOptions/IncDecVolumeOption/translations/incdecvolume_plugin_pt.ts
+++ src/plugins/CommandLineOptions/IncDecVolumeOption/translations/incdecvolume_plugin_pt.ts
@@ -1,17 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="pt">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pt" version="2.0">
 <context>
     <name>IncDecVolumeCommandLineOption</name>
     <message>
         <location filename="../incdecvolumeoption.cpp" line="44"/>
         <source>Increase volume with step 5</source>
-        <translation type="unfinished"></translation>
+        <translation>Aumentar volume com passo 5</translation>
     </message>
     <message>
         <location filename="../incdecvolumeoption.cpp" line="45"/>
         <source>Decrease volume with step 5</source>
-        <translation type="unfinished"></translation>
+        <translation>Diminuir volume com passo 5</translation>
     </message>
 </context>
 </TS>
