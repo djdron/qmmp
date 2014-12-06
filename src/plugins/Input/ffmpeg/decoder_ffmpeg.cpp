c0bb36a503869b5fb2e85767efdb63c81ecdc835
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-12-06 17:35:41 +0000

    fixed ffmpeg 2.5 support
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@4626 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/ffmpeg/decoder_ffmpeg.cpp
+++ src/plugins/Input/ffmpeg/decoder_ffmpeg.cpp
@@ -112,6 +112,7 @@ bool DecoderFFmpeg::initialize()
     ic = avformat_alloc_context();
 
     AVProbeData  pd;
+    memset(&pd, 0, sizeof(pd));
     uint8_t buf[PROBE_BUFFER_SIZE + AVPROBE_PADDING_SIZE];
     pd.filename = m_path.toLocal8Bit().constData();
     pd.buf_size = input()->peek((char*)buf, sizeof(buf) - AVPROBE_PADDING_SIZE);
