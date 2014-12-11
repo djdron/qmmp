29056df8c32e4bc4fceaf946a5ce89c1f2bdc7e1
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-12-11 08:48:03 +0000

    fixed regression in the hotkey plugin
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@4630 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/General/hotkey/hotkeymanager_x11.cpp
+++ qmmp/src/plugins/General/hotkey/hotkeymanager_x11.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009-2013 by Ilya Kotov                                 *
+ *   Copyright (C) 2009-2014 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   Copyright (C) 2003-2007 by Justin Karneges and Michail Pishchagin     *
@@ -316,4 +316,7 @@ quint32 HotkeyManager::keycodeToKeysym(quint32 keycode)
     return XKeycodeToKeysym(QX11Info::display(), keycode,0);
 #endif
 }
+
+#include "moc_hotkeymanager.cpp"
+
 #endif
