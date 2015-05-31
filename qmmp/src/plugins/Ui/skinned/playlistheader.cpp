a5dc34d24a49c4b864dc4852d44670dc8f8eb604
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-05-31 08:45:12 +0000

    skinned: fixed minimal column size
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@5056 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Ui/skinned/playlistheader.cpp
+++ qmmp/src/plugins/Ui/skinned/playlistheader.cpp
@@ -147,6 +147,9 @@ void PlayListHeader::setNumberWidth(int width)
     m_columns[0]->minSize = 30 + (width ? (width + 2 * m_pl_padding) : 0);
     int size = qMax(m_columns[0]->size, m_columns[0]->minSize);
 
+    for(int i = 1; i < m_columns.count(); ++i) //restore mimimal size for other columns
+        m_columns[i]->minSize = 30;
+
     if(width != m_number_width || size != m_columns[0]->size)
     {
         m_number_width = width;
