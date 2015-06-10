13cd9a0b300799d1b18c2dc9f13de56483dce2a7
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-06-10 12:07:24 +0000

    updated German translation (Ettore Atalan)
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@5119 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/translations/libqmmpui_pt.ts
+++ src/qmmpui/translations/libqmmpui_pt.ts
@@ -878,17 +878,17 @@
 <context>
     <name>PlayListHeaderModel</name>
     <message>
-        <location filename="../playlistheadermodel.cpp" line="35"/>
+        <location filename="../playlistheadermodel.cpp" line="36"/>
         <source>Artist - Title</source>
         <translation type="unfinished">Artista - Título</translation>
     </message>
     <message>
-        <location filename="../playlistheadermodel.cpp" line="179"/>
+        <location filename="../playlistheadermodel.cpp" line="186"/>
         <source>Title</source>
         <translation>Título</translation>
     </message>
     <message>
-        <location filename="../playlistheadermodel.cpp" line="180"/>
+        <location filename="../playlistheadermodel.cpp" line="187"/>
         <source>Add Column</source>
         <translation>Adicionar coluna</translation>
     </message>
