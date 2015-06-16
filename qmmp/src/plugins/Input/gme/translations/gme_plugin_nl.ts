87590f3ce26e80cea8d9e5631d6c034917bfb0aa
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-06-16 19:12:38 +0000

    updated .ts files
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@5166 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Input/gme/translations/gme_plugin_nl.ts
+++ qmmp/src/plugins/Input/gme/translations/gme_plugin_nl.ts
@@ -34,4 +34,27 @@
         <translation>Auteur: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
     </message>
 </context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>GME Plugin Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="34"/>
+        <source>Fadeout length:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="44"/>
+        <source>ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="57"/>
+        <source>Enable fadeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
