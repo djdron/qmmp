aa65a3f15166db380dfb0565f753c43bbe5fc124
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-07-01 18:23:58 +0000

    fixed normal container
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk/qmmp@5199 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmpui/playlistcontainer_p.h
+++ src/qmmpui/playlistcontainer_p.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013-2014 by Ilya Kotov                                 *
+ *   Copyright (C) 2013-2015 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -67,6 +67,9 @@ public:
 
     virtual void reverseList() = 0;
     virtual void randomizeList() = 0;
+
+protected:
+    void swapTrackNumbers(QList<PlayListItem *> *container, int index1, int index2);
 };
 
 #endif // PLAYLISTCONTAINER_P_H
