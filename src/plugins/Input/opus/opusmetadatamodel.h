49118064a26993f93f37cc7e6717e78597905866
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-08-05 06:23:37 +0000

    fixed build
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-1.0@5328 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Input/opus/opusmetadatamodel.h
+++ src/plugins/Input/opus/opusmetadatamodel.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013 by Ilya Kotov                                      *
+ *   Copyright (C) 2013-2015 by Ilya Kotov                                 *
  *   forkotov02@hotmail.ru                                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -21,7 +21,7 @@
 #ifndef OPUSMETADATAMODEL_H
 #define OPUSMETADATAMODEL_H
 
-#include "opusfile.h"
+#include <taglib/opusfile.h>
 #include <taglib/xiphcomment.h>
 #include <qmmp/metadatamodel.h>
 
