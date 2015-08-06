7c5268d5636071d97c65d7aad4e7c8d12c7c100d
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-08-06 18:59:22 +0000

    fixed build
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@5330 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/start_sh.cmd
+++ extras/package/win32/contrib/start_sh.cmd
@@ -1,2 +1,2 @@
-SET PATH=C:\devel\msys\bin;C:\Program Files (x86)\CMake\bin;C:\Program Files\CMake\bin
+SET PATH=C:\devel\msys\bin;C:\devel\mingw32\bin;C:\Program Files (x86)\CMake\bin;C:\Program Files\CMake\bin
 sh.exe
