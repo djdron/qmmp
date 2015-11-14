6cf5602068bfd047a434374f590d6a208b39676f
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-11-14 08:02:22 +0000

    updated rusxmms patch
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@5771 90c681e8-e032-0410-971d-27865f9a5e38

--- extras/package/win32/contrib/taglib-rusxmms/taglib-rusxmms.sh
+++ extras/package/win32/contrib/taglib-rusxmms/taglib-rusxmms.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 NAME=taglib
-VERSION=1.9.1
+VERSION=1.10
 
 case $1 in
   --download)
     mkdir -p temp
     cd temp
     wget -nc http://taglib.github.io/releases/$NAME-$VERSION.tar.gz
-    wget -nc http://darksoft.org/files/rusxmms/patches/taglib-csa10.tar.bz2
+    #wget -nc http://darksoft.org/files/rusxmms/patches/taglib-csa10.tar.bz2
   ;;
   --install)
     cd temp
     tar xvzf $NAME-$VERSION.tar.gz
-    tar xvjf taglib-csa10.tar.bz2
     cd $NAME-$VERSION
-    cat ../../taglib-1.9.1-bug-308.diff | patch -p1 --verbose
-    cat ../taglib/taglib-1.9.1-ds-rusxmms.patch | patch -p1 --verbose
+    cat ../../taglib-1.10-ds-rusxmms.patch | patch -p1 --verbose
     cmake ./ -DCMAKE_INSTALL_PREFIX=${PREFIX}/taglib-rusxmms -G "MSYS Makefiles" -DZLIB_ROOT=${ZLIB_ROOT} \
     -DCMAKE_COLOR_MAKEFILE:BOOL=OFF -DCMAKE_CXX_FLAGS="-I${PREFIX}/include" -DCMAKE_SHARED_LINKER_FLAGS="-L${PREFIX}/lib"
     mingw32-make -j${JOBS}
