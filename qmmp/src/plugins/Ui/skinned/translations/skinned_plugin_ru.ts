903a3acf8428ed7c1a4ee87a00ecd92051ebdb6f
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-12-05 17:52:38 +0000

    updated Russian translation
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/trunk@5813 90c681e8-e032-0410-971d-27865f9a5e38

--- qmmp/src/plugins/Ui/skinned/translations/skinned_plugin_ru.ts
+++ qmmp/src/plugins/Ui/skinned/translations/skinned_plugin_ru.ts
@@ -176,7 +176,7 @@
     <message>
         <location filename="../actionmanager.cpp" line="59"/>
         <source>Anti-aliasing</source>
-        <translation type="unfinished"></translation>
+        <translation>Сглаживание</translation>
     </message>
     <message>
         <location filename="../actionmanager.cpp" line="61"/>
