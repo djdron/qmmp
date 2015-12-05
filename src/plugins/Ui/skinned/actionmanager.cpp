1b9150f9d43734fdfbe78019cee11330a455bb93
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-12-05 18:12:34 +0000

    skinned: disable Anti-aliasing action on normal size
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-1.1@5816 90c681e8-e032-0410-971d-27865f9a5e38

--- src/plugins/Ui/skinned/actionmanager.cpp
+++ src/plugins/Ui/skinned/actionmanager.cpp
@@ -57,6 +57,8 @@ ActionManager::ActionManager(QObject *parent) :
     m_actions[WM_STICKY] = createAction2(tr("Put on All Workspaces"), "sticky", "");
     m_actions[WM_DOUBLE_SIZE] = createAction2(tr("Double Size"), "double_size", tr("Meta+D"));
     m_actions[WM_ANTIALIASING] = createAction2(tr("Anti-aliasing"), "anti_aliasing", "");
+    connect(m_actions[WM_DOUBLE_SIZE], SIGNAL(toggled(bool)), m_actions[WM_ANTIALIASING], SLOT(setEnabled(bool)));
+    m_actions[WM_ANTIALIASING]->setEnabled(false);
     //volume
     m_actions[VOL_ENC] = createAction(tr("Volume &+"), "vol_enc", tr("0"));
     m_actions[VOL_DEC] = createAction(tr("Volume &-"), "vol_dec", tr("9"));
