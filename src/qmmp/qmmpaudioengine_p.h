51c0c4e0cf99a8569e926ccbb3d1798887a28796
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-12-13 16:26:51 +0000

    fixed gcc warning
    git-svn-id: svn://svn.code.sf.net/p/qmmp-dev/code/branches/qmmp-1.1@5825 90c681e8-e032-0410-971d-27865f9a5e38

--- src/qmmp/qmmpaudioengine_p.h
+++ src/qmmp/qmmpaudioengine_p.h
@@ -82,7 +82,7 @@ private:
     bool m_done, m_finish, m_user_stop;
     uint m_bks;
     qint64 m_seekTime;
-    qint64 m_output_at, m_output_size;
+    quint64 m_output_at, m_output_size;
     int m_bitrate;
     unsigned char *m_output_buf;
     Decoder *m_decoder;
